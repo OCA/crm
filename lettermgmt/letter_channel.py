8676fa6429dfd55dbb1426902694b587988a9ccb
unknown <sandy.carter@savoirfairelinux.com>
2014-03-24 12:05:52 +0100

    [ADD] lettermanagement module with automatic hr bindings module

---
+++ lettermgmt/letter_channel.py
@@ -0,0 +1,34 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Parthiv Pate, Tech Receptives, Open Source For Ideas
+#    Copyright (C) 2009-Today Tech Receptives(http://techreceptives.com).
+#    All Rights Reserved
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
+
+from openerp.osv import fields, orm
+from openerp.tools.translate import _
+
+
+class letter_channel(orm.Model):
+    """ Class to define various channels using which letters can be sent or received like : post, fax, email. """
+    _name = 'letter.channel'
+    _description = _("Send/Receive channel")
+    _columns = {
+        'name': fields.char('Type', size=32, required=True),
+    }
---
+++ lettermgmt/letter_reassignment.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from openerp.osv import fields, orm
+from openerp.tools.translate import _
+
+
+class letter_reassignment(orm.Model):
+    """A line to forward a letter with a comment"""
+    _name = 'letter.reassignment'
+    _description = _('Reassignment line')
+    _columns = {
+        'name': fields.many2one('res.users', string='Name', help='User to forward letter to.'),
+        'comment': fields.text('Comment', help='Comment for user explaining forward.'),
+        'letter_id': fields.many2one('res.letter', string='Letter', help='Letter in question.'),
+    }
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
