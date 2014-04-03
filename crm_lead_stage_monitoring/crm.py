06026fe1e9c2f3890308f95ce705d371739d592b
Vincent Renaville <vincent.renaville@camptocamp.com>
2014-04-03 08:59:17 +0200

    [FIX] test for default dict

--- crm_lead_stage_monitoring/crm.py
+++ crm_lead_stage_monitoring/crm.py
@@ -39,7 +39,7 @@ class CrmLead(orm.Model):
         return super(CrmLead, self).write(cr, uid, ids, vals, context=context)
 
     def copy(self, cr, uid, id, default=None, context=None):
-        if not default:
+        if default is None:
             default = {}
         ## Do not duplicate last_activity_stage
         default['last_activity_stage'] = False
