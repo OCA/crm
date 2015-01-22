fcddd63f389f21c7e1890639e061e65c0d4c0b5b
Holger Brunn <hbrunn@therp.nl>
2015-01-22 10:14:28 +0100

    [DEL] placeholder [ADD] preliminary migration of lettermgmt

---
+++ lettermgmt/__openerp__.py
@@ -0,0 +1,67 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'Letter Management',
+    'version': '0.2',
+    'author': 'Savoir-faire Linux',
+    'maintainer': 'Savoir-faire Linux',
+    'website': 'http://www.savoirfairelinux.com',
+    'license': 'AGPL-3',
+    'category': 'Social Network',
+    'summary': 'Track letters, parcels, registered documents',
+    'description': """
+Letter Management
+=================
+
+Using this module you can track Incoming / Outgoing letters, parcels,
+registered documents or any other paper documents that are important for your
+company to keep track of.
+
+Contributors
+------------
+* Sandy Carter <sandy.carter@savoirfairelinux.com>
+* Parthiv Patel, Tech Receptives (Original 6.0 Author)
+
+""",
+    'depends': ['mail'],
+    'external_dependencies': {
+        'python': [],
+    },
+    'data': [
+        "views/res_letter_view.xml",
+        "views/letter_folder_view.xml",
+        "views/letter_channel_view.xml",
+        "views/letter_class_view.xml",
+        "views/letter_history_view.xml",
+        "views/letter_reassignment_view.xml",
+        "views/letter_type_view.xml",
+        "data/letter_sequence.xml",
+        'security/ir.model.access.csv',
+    ],
+    'demo': ["data/letter_demo.xml"],
+    'test': [],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ lettermgmt/models/letter_folder.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from openerp.osv import orm, fields
+from openerp.tools.translate import _
+
+
+class letter_folder(orm.Model):
+    """Folder which contains collections of letters"""
+    _name = 'letter.folder'
+    _description = _('Letter Folder')
+    _columns = {
+        'name': fields.char('Name', required=True),
+        'code': fields.char('Code', size=8, required=True),
+        'letter_ids': fields.one2many(
+            'res.letter', 'folder_id', string='Letters',
+            help='Letters contained in this folder.'),
+    }
+    _sql_constraints = [('code_uniq', 'unique(code)', 'Code must be unique !')]
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
