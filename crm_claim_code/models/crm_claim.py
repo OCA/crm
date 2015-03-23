9f97239888aeef4f38e05f8ad19a75ab9d5bf07e
oihane <oihanecruce@gmail.com>
2015-03-23 17:22:21 +0100

    [ADD] crm_claim_code

---
+++ crm_claim_code/models/crm_claim.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+from openerp import models, fields, api
+
+
+class CrmClaim(models.Model):
+    _inherit = "crm.claim"
+
+    code = fields.Char(
+        string='Claim Number', required=True, default="/", readonly=True)
+
+    _sql_constraints = [
+        ('crm_claim_unique_code', 'UNIQUE (code)',
+         'The code must be unique!'),
+    ]
+
+    @api.model
+    def create(self, vals):
+        if vals.get('code', '/') == '/':
+            vals['code'] = self.env['ir.sequence'].get('crm.claim')
+        return super(CrmClaim, self).create(vals)
+
+    @api.one
+    def copy(self, default=None):
+        if default is None:
+            default = {}
+        default['code'] = self.env['ir.sequence'].get('crm.claim')
+        return super(CrmClaim, self).copy(default)
