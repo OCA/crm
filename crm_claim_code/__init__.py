10482193a1fcf74ebf5dd96e4593ac28517e87a2
oihane <oihanecruce@gmail.com>
2015-04-01 13:06:12 +0200

    [MOD] Post_init_hook added but does not work properly

--- crm_claim_code/__init__.py
+++ crm_claim_code/__init__.py
@@ -8,10 +8,22 @@ from openerp import SUPERUSER_ID
 
 
 def assign_old_sequences(cr, registry):
-    claim_obj = registry['crm.claim']
-    sequence_obj = registry['ir.sequence']
-    claim_ids = claim_obj.search(cr, SUPERUSER_ID, [], order="id")
-    for claim_id in claim_ids:
-        claim_obj.write(cr, SUPERUSER_ID, claim_id,
-                        {'code': sequence_obj.get(
-                            cr, SUPERUSER_ID, 'crm.claim')})
+    cr.execute("""
+        SELECT True
+        FROM information_schema.columns
+        WHERE table_name='crm_claim' AND column_name='sequence'
+    """)
+    found = cr.fetchall()
+    if found and found[0]:
+        cr.execute("""
+            UPDATE crm_claim
+            SET code = sequence
+            WHERE code IS NULL OR code = '/'
+        """)
+    # claim_obj = registry['crm.claim']
+    # sequence_obj = registry['ir.sequence']
+    # claim_ids = claim_obj.search(cr, SUPERUSER_ID, [], order="id")
+    # for claim_id in claim_ids:
+    #    claim_obj.write(cr, SUPERUSER_ID, claim_id,
+    #                     {'code': sequence_obj.get(
+    #                         cr, SUPERUSER_ID, 'crm.claim')})
