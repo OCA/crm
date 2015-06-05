a78a35b2bb6411eee46b48a793062444e6d8b4a6
Endika Iglesias <endikaig@antiun.com>
2015-06-05 08:37:42 +0200

    [FIX]

--- project_recalculate/models/project_project.py
+++ project_recalculate/models/project_project.py
@@ -56,10 +56,7 @@ class ProjectProject(models.Model):
             for project_task in project_task_ids:
                 date_start = fields.Datetime.from_string(
                     project_task.date_start)
-                date_end = (fields.Datetime.from_string(project_task.date_end)
-                            if project_task.date_end else
-                            fields.Datetime.from_string(
-                                project_task.date_start))
+                date_end = fields.Datetime.from_string(project_task.date_end)
                 if min_date_start > date_start:
                     min_date_start = date_start
                     date_start_from_days = project_task.from_days
