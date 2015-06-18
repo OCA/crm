95045afe7bc8682b5ca6473fce8ca8ae9bcecb62
oihane <oihanecruce@gmail.com>
2015-06-18 16:18:57 +0200

    [IMP] <crm_claim_code> Cleaned README and unnecessary code in init

--- crm_claim_code/__init__.py
+++ crm_claim_code/__init__.py
@@ -20,10 +20,3 @@ def assign_old_sequences(cr, registry):
             SET code = sequence
             WHERE code IS NULL OR code = '/'
         """)
-    # claim_obj = registry['crm.claim']
-    # sequence_obj = registry['ir.sequence']
-    # claim_ids = claim_obj.search(cr, SUPERUSER_ID, [], order="id")
-    # for claim_id in claim_ids:
-    #    claim_obj.write(cr, SUPERUSER_ID, claim_id,
-    #                     {'code': sequence_obj.get(
-    #                         cr, SUPERUSER_ID, 'crm.claim')})
