bf47ccd1ca79ed89ad8e8b69758efba056bb7e98
osval <osval@vauxoo.com>
2015-08-08 10:47:50 -0430

    [REF] crm_claim_type: update license header agreements

--- crm_claim_type/__openerp__.py
+++ crm_claim_type/__openerp__.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#    Module Writen to OpenERP, Open Source Management Solution
-#    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
-#    All Rights Reserved
-# ############ Credits ########################################################
-#    Coded by: Yanina Aular <yani@vauxoo.com>
-#    Planified by: Yanina Aular <yani@vauxoo.com>
-#    Audited by: Moises Lopez <moylop260@vauxoo.com>
-#                Nhomar Hernandez <nhomar@vauxoo.com>
-# ##############################################################################
+##############################################################################
+#
+#    Copyright 2015 Vauxoo
+#    Author : Yanina Aular <yani@vauxoo.com>
+#             Osval Reyes <osval@vauxoo.com>
+#
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published
 #    by the Free Software Foundation, either version 3 of the License, or
@@ -21,7 +17,8 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# ##############################################################################
+#
+##############################################################################
 {
     'name': 'CRM Claim Types',
     'category': 'Customer Relationship Management',
@@ -34,10 +31,10 @@
         'crm_claim',
     ],
     'data': [
+        'data/crm_claim_type_data.xml',
         'security/ir.model.access.csv',
         'views/crm_claim_type_view.xml',
         'views/crm_case_claims_form_view.xml',
-        'data/crm_claim_type_data.xml',
     ],
     'installable': True,
     'auto_install': False,
