0662619fac28ad4920a1ee3bb33cd70cb6b7df4b
Javier Iniesta <javieria@antiun.com>
2015-08-28 11:15:50 +0200

    [ADD] mass_mailing_partner

---
+++ mass_mailing_partner/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# License AGPL-3 - See LICENSE file on root folder for details
+##############################################################################
+
+from . import mail_mass_mailing_contact
+from . import res_partner
+from . import mail_mass_mailing
