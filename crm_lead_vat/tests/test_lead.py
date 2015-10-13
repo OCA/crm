933d39c970d9200406275e717c58170c0f4da964
Jairo Llopis <yajo.sk8@gmail.com>
2015-10-13 16:09:15 +0200

    Drop dependency on https://github.com/odoo/odoo/pull/8899.

--- crm_lead_vat/tests/test_lead.py
+++ crm_lead_vat/tests/test_lead.py
@@ -8,19 +8,22 @@ from openerp.tests.common import TransactionCase
 class LeadCase(TransactionCase):
     def setUp(self):
         super(LeadCase, self).setUp()
-        self.lead = self.env["crm.lead"].create({"name": __file__})
+        self.lead = self.env["crm.lead"].create({
+            "name": __file__,
+            "partner_name": u"HÎ"
+        })
         self.partner = self.env["res.partner"].create({"name": __file__})
-        self.test_vat = "ES98765432M"
+        self.test_field = "ES98765432M"
 
-    def test_mapped_values(self):
-        """VAT gets mapped when creating partner."""
-        self.lead.vat = self.test_vat
-        mapped = self.lead._map_values_to_partner(self.lead.name, False)[0]
-        self.assertEqual(mapped["vat"], self.test_vat)
+    def test_transfered_values(self):
+        """Field gets transfered when creating partner."""
+        self.lead.vat = self.test_field
+        self.lead.handle_partner_assignation()
+        self.assertEqual(self.lead.partner_id.vat, self.test_field)
 
     def test_onchange_partner_id(self):
         """Lead gets VAT from partner when linked to it."""
-        self.partner.vat = self.test_vat
+        self.partner.vat = self.test_field
         self.lead.partner_id = self.partner
         result = self.lead.on_change_partner_id(self.partner.id)
-        self.assertEqual(result["value"]["vat"], self.test_vat)
+        self.assertEqual(result["value"]["vat"], self.test_field)
