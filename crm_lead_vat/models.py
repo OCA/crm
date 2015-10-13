cb27fb2805b34e0f63102143753b7a4786a3c613
Jairo Llopis <yajo.sk8@gmail.com>
2015-10-13 16:09:15 +0200

    Drop dependency on https://github.com/odoo/odoo/pull/8899.

--- crm_lead_vat/models.py
+++ crm_lead_vat/models.py
@@ -13,12 +13,11 @@ class Lead(models.Model):
         help="Tax Identification Number. The first 2 characters are the "
              "country code.")
 
-    @api.one
-    def _map_values_to_partner(self, *args, **kwargs):
-        """Add VAT to mapped values."""
-        result = super(Lead, self)._map_values_to_partner(*args, **kwargs)[0]
-        result["vat"] = self.vat
-        return result
+    @api.model
+    def _lead_create_contact(self, lead, name, is_company, parent_id=False):
+        """Add VAT to partner."""
+        return (super(Lead, self.with_context(default_vat=lead.vat))
+                ._lead_create_contact(lead, name, is_company, parent_id))
 
     def on_change_partner_id(self, cr, uid, ids, partner_id, context=None):
         """Recover VAT from partner if available."""
