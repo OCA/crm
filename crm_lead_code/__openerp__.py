d447717c0a4f15cdc592e02e49db30ad360a253d
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-10-14 03:12:56 +0200

    [MIG] Make modules uninstallable

--- crm_lead_code/__openerp__.py
+++ crm_lead_code/__openerp__.py
@@ -42,7 +42,7 @@
         "views/crm_lead_view.xml",
         "data/lead_sequence.xml",
     ],
-    "installable": True,
+    'installable': False,
     "pre_init_hook": "create_code_equal_to_id",
     "post_init_hook": "assign_old_sequences",
 }
