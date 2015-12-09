04544c0c342b1f6a152c7a47055decc617ebbd61
Javier Iniesta <javieria@antiun.com>
2015-12-09 10:49:14 +0100

    crm_sector

---
+++ crm_sector/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# © 2015 Antiun Ingenieria S.L. - Javier Iniesta
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "CRM Sector",
+    "summary": "Link leads/opportunities to sectors",
+    "version": "8.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "website": "http://www.antiun.com",
+    "author": "Antiun Ingeniería, "
+              "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "crm",
+        "partner_sector",
+    ],
+    "data": [
+        "views/crm_lead_view.xml",
+    ]
+}
