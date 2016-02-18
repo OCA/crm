4f5c4d81c2922390cbc41f77d77be52892d3feae
Jairo Llopis <yajo.sk8@gmail.com>
2016-02-18 18:28:37 +0100

    Fix travis.

--- crm_deduplicate_acl/wizards/partner_merge.py
+++ crm_deduplicate_acl/wizards/partner_merge.py
@@ -2,7 +2,7 @@
 # © 2016 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import SUPERUSER_ID, api, fields, models
+from openerp import SUPERUSER_ID, api, models
 
 
 class BasePartnerMergeAutomaticWizard(models.TransientModel):
