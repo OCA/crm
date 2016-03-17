81ceed6999799a09f88fac552a51c53d7965eed2
Iván Todorovich <ivan.todorovich@gmail.com>
2016-03-16 22:31:43 -0300

    Refactored api to v8. Updated py headers. Added headers to xml files. Reordered res_letter.py lines to make them more readable. Improved help strings on a few fields. Added default values for sender/recipient. Simplified views. Unified 'in' and 'out' letter views. Major view improvements. Changed class to category_ids (Many2many_tags). Changed type to type_id (for consistency). Added migration script. Added view's 'oe_view_nocontent_create' helps for types, categories, channels and folders. Add security group to hide/show reassignments and parent/child. Increment sequence only after the record is saved, not before. Simplify and improve workflow. Known Issue: 'created' and 'validated' states are reverted to 'draft'. Added search filter 'Pending' and set by default (shows letters that haven't arrived to destination). Make 'recipient' and 'sender' fields required in the model, instead of required only in the view. Add tests

--- lettermgmt/__openerp__.py
+++ lettermgmt/__openerp__.py
@@ -1,52 +1,31 @@
-# -*- encoding: utf-8 -*-
-###############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2013 Savoir-faire Linux
-#    (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
+# -*- coding: utf-8 -*-
+# © 2016 Iván Todorovich <ivan.todorovich@gmail.com>
+# © 2015 Holger Brunn <hbrunn@therp.nl>
+# © 2009 Sandy Carter <sandy.carter@savoirfairelinux.com>
+# © 2009 Parthiv Patel, Tech Receptives
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     'name': 'Letter Management',
-    'version': '8.0.1.2.0',
-    'author': "Savoir-faire Linux,Odoo Community Association (OCA)",
-    'maintainer': 'Savoir-faire Linux',
-    'website': 'http://www.savoirfairelinux.com',
-    'license': 'AGPL-3',
-    'category': 'Customer Relationship Management',
     'summary': 'Track letters, parcels, registered documents',
-    'depends': ['mail'],
-    'external_dependencies': {
-        'python': [],
-    },
-    'data': [
-        "views/res_letter_view.xml",
-        "views/letter_folder_view.xml",
-        "views/letter_channel_view.xml",
-        "views/letter_class_view.xml",
-        "views/letter_reassignment_view.xml",
-        "views/letter_type_view.xml",
-        "data/letter_sequence.xml",
-        'security/ir.model.access.csv',
-    ],
-    'demo': ["data/letter_demo.xml"],
-    'test': [],
+    'version': '8.0.2.0.0',
+    'category': 'Customer Relationship Management',
+    'website': 'https://odoo-community.org/',
+    'author': 'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'application': False,
     'installable': True,
-    'auto_install': False,
+    'depends': ['mail'],
+    'data': [
+        'views/res_letter_view.xml',
+        'views/letter_category_view.xml',
+        'views/letter_type_view.xml',
+        'views/letter_channel_view.xml',
+        'views/letter_folder_view.xml',
+        'views/letter_reassignment_view.xml',
+        'data/letter_sequence.xml',
+        'security/ir.model.access.csv',
+        'security/lettermgmt_security.xml',
+    ],
+    'demo': ['data/letter_demo.xml'],
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
