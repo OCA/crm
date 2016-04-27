43a18d967d41e342a7e62c29ceb8aab5f092c394
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-04-27 15:32:03 +0200

    crm_claim_type: migration v9

--- crm_claim_type/__openerp__.py
+++ crm_claim_type/__openerp__.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright 2015 Vauxoo
-#    Author : Yanina Aular <yani@vauxoo.com>
-#             Osval Reyes <osval@vauxoo.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Vauxoo: Yanina Aular <yani@vauxoo.com>, Osval Reyes <osval@vauxoo.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
     'name': 'CRM Claim Types',
     'category': 'Customer Relationship Management',
@@ -27,7 +10,7 @@
               'Vauxoo',
     'website': 'www.vauxoo.com',
     'license': 'AGPL-3',
-    'version': '8.0.1.0.0',
+    'version': '9.0.1.0.0',
     'depends': [
         'crm_claim',
     ],
