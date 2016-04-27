e37bb20cabb622e0ad61af428110a68309fc85d1
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-04-27 15:32:03 +0200

    crm_claim_type: migration v9

--- crm_claim_type/models/crm_claim_stage.py
+++ crm_claim_type/models/crm_claim_stage.py
@@ -1,43 +1,20 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright 2015 Vauxoo
-#    Copyright 2013 Camptocamp
-#    Copyright 2009-2013 Akretion,
-#    Author: Emmanuel Samyn, Raphaël Valyi, Sébastien Beau,
-#            Benoît Guillot, Joel Grand-Guillaume,
-#            Osval Reyes, Yanina Aular
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Vauxoo
+# © 2013 Camptocamp
+# © 2009-2013 Akretion
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from openerp import fields, models
 
 
 class CrmClaimStage(models.Model):
 
     _inherit = 'crm.claim.stage'
 
-    @api.model
-    def _get_claim_type(self):
-        return self.env['crm.claim']._get_claim_type()
-
-    claim_type = \
-        fields.Many2one('crm.claim.type',
-                        selection=_get_claim_type,
-                        help="Claim classification")
+    claim_type = fields.Many2one(
+        'crm.claim.type',
+        help="Claim classification"
+    )
 
     claim_common = fields.Boolean(string='Common to All Claim Types',
                                   help="If you check this field,"
