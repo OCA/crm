ba2b8f3c7b669f79763f1e4c54c35283d27b3a6a
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-04-27 15:32:03 +0200

    crm_claim_type: migration v9

--- crm_claim_type/models/crm_claim.py
+++ crm_claim_type/models/crm_claim.py
@@ -1,24 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Copyright 2015 Vauxoo
-#    Author : Yanina Aular <yani@vauxoo.com>
-#             Osval Reyes <osval@vauxoo.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 Vauxoo: Yanina Aular <yani@vauxoo.com>, Osval Reyes <osval@vauxoo.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp import fields, models
 
 
@@ -29,14 +12,14 @@ class CrmClaim(models.Model):
     """
     _inherit = 'crm.claim'
 
-    claim_type = \
-        fields.Many2one('crm.claim.type',
-                        help="Claim classification")
+    claim_type = fields.Many2one('crm.claim.type', help="Claim classification")
 
-    stage_id = fields.Many2one('crm.claim.stage',
-                               'Stage',
-                               track_visibility='onchange',
-                               domain="[ '&','|',('section_ids', '=', "
-                               "section_id), ('case_default', '=', True), "
-                               "'|',('claim_type', '=', claim_type)"
-                               ",('claim_common', '=', True)]")
+    stage_id = fields.Many2one(
+        'crm.claim.stage',
+        string='Stage',
+        track_visibility='onchange',
+        domain="[ '&',"
+               "'|',('team_ids', '=', team_id), "
+               "('case_default', '=', True), "
+               "'|',('claim_type', '=', claim_type)"
+               ",('claim_common', '=', True)]")
