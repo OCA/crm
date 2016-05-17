7773fa2a699bc391c3f86fb964895c535cde85f6
Aurélien Rivet <aurelienrivet@hotmail.fr>
2016-05-17 11:11:45 +0200

    New module: phonecaall_plan.
    This module provides a wizard to plan calls from partners/leads views.

---
+++ phonecall_plan/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2016 6IT
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Phonecall plan',
+    'summary': "Wizard to plan calls from leads/partners",
+    'version': '8.0.1.0.0',
+    'depends': ['base', 'crm'],
+    'author': 'Aurélien Rivet, 6IT, Odoo Community Association (OCA)',
+    'category': 'Customer Relationship Management',
+    'website': 'https://6it.fr',
+    'license': 'AGPL-3',
+    'data': ['wizards/phonecall_plan_view.xml'],
+    'installable': True,
+    'active': False,
+}
---
+++ phonecall_plan/wizards/phonecall_plan.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+# © 2016 6IT
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api, _
+
+
+class AddCall(models.TransientModel):
+    _name = 'add.call'
+
+    salesperson_id = fields.Many2one(
+        'res.users',
+        string='Salesperson',
+        required=True,
+    )
+
+    date = fields.Date('Date')
+
+    lead_ids = fields.Many2many(
+        'crm.lead',
+        string='Leads',
+        default=lambda self: self._default_leads(),
+    )
+
+    def _default_lead_ids(self):
+        return self.env['crm.lead'].browse(self._context.get('active_ids'))
+
+    @api.multi
+    @api.depends('lead_ids')
+    def action_add_call(self):
+        obj_phone = self.env['crm.phonecall']
+        obj_lead = self.env['crm.lead']
+        for lead in self.lead_ids:
+
+            obj_phone.create(
+                {
+                    'date': self.date,
+                    'name': lead.name,
+                    'partner_id': lead.partner_id.id,
+                    'user_id': self.salesperson_id.id,
+                    'partner_mobile': lead.mobile,
+                    'partner_phone': lead.phone,
+                    'opportunity_id': lead.id,
+                    'state': "open",
+
+                }
+            )
+            obj_lead.browse(lead.id).write(
+                {
+                    'user_id': self.salesperson_id.id
+                }
+            )
+        return {}
+
+
+class AddPartnerCall(models.TransientModel):
+    _name = 'add.partner_call'
+
+    salesperson_id = fields.Many2one(
+        'res.users',
+        string='Salesperson',
+        required=True,
+    )
+
+    date = fields.Date('Date')
+
+    partner_ids = fields.Many2many(
+        'res.partner',
+        string='Partners',
+        default=lambda self: self._default_partner_ids(),
+    )
+
+    def _default_partner_ids(self):
+        return self.env['res.partner'].browse(self._context.get('active_ids'))
+
+    @api.multi
+    @api.depends('partner_ids')
+    def action_add_call(self):
+        obj_phone = self.env['crm.phonecall']
+        for partner in self.partner_ids:
+            obj_phone.create(
+                {
+                    'date': self.date,
+                    'name': _('To fill !!!'),
+                    'partner_id': partner.id,
+                    'partner_phone': partner.phone,
+                    'partner_mobile': partner.mobile,
+                    'user_id':  self.salesperson_id.id,
+                    'state': "open",
+                })
+        return {}
