49391e933f8f9a926a3c3d6ffe72e4426b891eea
Alexis de Lattre <alexis.delattre@akretion.com>
2016-06-08 15:38:34 +0200

    Several technical and usability changes:
    Technical changes:
    - crm_action_type: rename field is_active to active
    - crm_action: rename field action_type (M2O) to action_type_id
    - remove specific groups: the native CRM groups are enough
    - fix dependancy: this module only depends on crm, not no sale_crm
    - update to the latest OCA code conventions
    
    Usability changes:
    - add buttons in actions tree view
    - rename "draft" to "todo" in state and button labels
    - replace next action on opportunity by a link to the next crm.action and add a button to mark it as done directly from the opportunity form view
    - add calendar view on actions
    - add groupby on crm.action search view
    - add filter "Todo Now" on crm.action search view
    - update README.rst

--- crm_action/models/crm_action_type.py
+++ crm_action/models/crm_action_type.py
@@ -1,24 +1,6 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2015 Savoir-faire Linux
-#    (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015-2016 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields
 
@@ -29,6 +11,6 @@ class CrmActionType(models.Model):
 
     name = fields.Char('Name', translate=True, required=True)
     priority = fields.Integer('Priority', required=True, default=0)
-    is_active = fields.Boolean('Active', default=True)
+    active = fields.Boolean('Active', default=True)
 
     _order = 'priority'
