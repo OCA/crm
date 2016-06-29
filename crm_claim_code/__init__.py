09065af28dcc92adaec4ded4839de2d0d1405a08
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2016-06-29 15:58:26 +0200

    Fix creation of existing column
    When a database already have this field, the module cannot be installed.
    Now it only creates the code column if it doesn't exist yet.
    
    Do not override existing values of crm_claim.code
    
    If the field was there before the installation of the addon

--- crm_claim_code/__init__.py
+++ crm_claim_code/__init__.py
@@ -8,14 +8,25 @@ from openerp.api import Environment
 from openerp import SUPERUSER_ID
 
 
+new_field_code_added = False
+
+
 def create_code_equal_to_id(cr):
-    cr.execute('ALTER TABLE crm_claim '
-               'ADD COLUMN code character varying;')
-    cr.execute('UPDATE crm_claim '
-               'SET code = id;')
+    cr.execute("SELECT column_name FROM information_schema.columns "
+               "WHERE table_name = 'crm_claim' AND column_name = 'code'")
+    if not cr.fetchone():
+        cr.execute('ALTER TABLE crm_claim '
+                   'ADD COLUMN code character varying;')
+        cr.execute('UPDATE crm_claim '
+                   'SET code = id;')
+        global new_field_code_added
+        new_field_code_added = True
 
 
 def assign_old_sequences(cr, registry):
+    if not new_field_code_added:
+        # the field was already existing before the installation of the addon
+        return
     with Environment.manage():
         env = Environment(cr, SUPERUSER_ID, {})
 
