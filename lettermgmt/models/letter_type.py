442d6a404b891230a585f3727a0430082c4951cb
Hardikgiri Goswami <hardikgiri.goswami@gmail.com>
2016-09-29 19:11:28 +0530

    [MIG] crm:lettermgmt Migrated Module to V9.

--- lettermgmt/models/letter_type.py
+++ lettermgmt/models/letter_type.py
@@ -1,35 +1,22 @@
 # -*- encoding: utf-8 -*-
-##############################################################################
-#
-#    Parthiv Pate, Tech Receptives, Open Source For Ideas
-#    Copyright (C) 2009-Today Tech Receptives(http://techreceptives.com).
-#    All Rights Reserved
-#
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see http://www.gnu.org/licenses/.
-#
-##############################################################################
-from openerp.osv import fields, orm
+# Copyright (C) 2009-Today Tech Receptives(http://techreceptives.com).
+# Parthiv Pate, Tech Receptives, Open Source For Ideas
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
 
 
-class letter_type(orm.Model):
+class LetterType(models.Model):
     """Class to define various types for letters like : envelope,parcel,
     etc."""
     _name = 'letter.type'
     _description = "Letter Type"
-    _columns = {
-        'name': fields.char('Type', required=True),
-        'code': fields.char('Code', required=True),
-    }
-    _sql_constraints = [('code_uniq', 'unique(code)', 'Code must be unique !')]
+
+    name = fields.Char('Type', required=True)
+    code = fields.Char('Code', required=True)
+
+    _sql_constraints = [
+        ('code_uniq', 'unique(code)', 'Code must be unique !')
+    ]
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
