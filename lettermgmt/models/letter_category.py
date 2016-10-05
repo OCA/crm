8034611da52babc4346c72ffdd91cf3db1a9cb89
Hardikgiri Goswami <hardikgiri.goswami@gmail.com>
2016-10-05 09:08:14 +0200

    [MIG]Migrated module, created this new PR as mentioned in PR #112.

--- lettermgmt/models/letter_category.py
+++ lettermgmt/models/letter_category.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 # Copyright (C) 2009-Today Tech Receptives(http://techreceptives.com).
 # Parthiv Pate, Tech Receptives, Open Source For Ideas
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
@@ -6,10 +6,9 @@
 from openerp import models, fields
 
 
-class LetterChannel(models.Model):
-    """ Class to define various channels using which letters can be sent or
-    received like : post, fax, email. """
-    _name = 'letter.channel'
-    _description = "Send/Receive channel"
-
+class LetterCategory(models.Model):
+    """ Class to define the category of letter like : classified,
+    confidential, personal, etc. """
+    _name = 'letter.category'
+    _description = "Letter Category"
     name = fields.Char('Type', required=True)
--- lettermgmt/models/letter_reassignment.py
+++ lettermgmt/models/letter_reassignment.py
@@ -1,38 +1,20 @@
-# -*- encoding: utf-8 -*-
-###############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2013 Savoir-faire Linux
-#    (<http://www.savoirfairelinux.com>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
-from openerp.osv import fields, orm
+# -*- coding: utf-8 -*-
+# copyright (C) 2013 Savoir-faire Linux <http://www.savoirfairelinux.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
 
 
-class letter_reassignment(orm.Model):
+class LetterReassignment(models.Model):
     """A line to forward a letter with a comment"""
     _name = 'letter.reassignment'
     _description = 'Reassignment line'
-    _columns = {
-        'name': fields.many2one(
-            'res.users', string='Name', help='User to forward letter to.'),
-        'comment': fields.text(
-            'Comment', help='Comment for user explaining forward.'),
-        'letter_id': fields.many2one(
-            'res.letter', string='Letter', help='Letter in question.'),
-    }
+
+    name = fields.Many2one(
+        'res.users', string='Name', help='User to forward letter to.')
+    comment = fields.Text(
+        'Comment', help='Comment for user explaining forward.')
+    letter_id = fields.Many2one(
+        'res.letter', string='Letter', help='Letter in question.')
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
