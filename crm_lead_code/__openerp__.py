0462eee451d018073b72daa0216f67aef4ad6ada
Atul Arvind <atul.surficle@gmail.com>
2016-10-21 19:25:27 +0530

    [IMP] __openerp__.py file

--- crm_lead_code/__openerp__.py
+++ crm_lead_code/__openerp__.py
@@ -1,25 +1,8 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-#
-# Copyright (c)
-#    2015 Serv. Tec. Avanzados - Pedro M. Baeza (http://www.serviciosbaeza.com)
-#    2015 AvanzOsc (http://www.avanzosc.es)
-#    2016 Atul Arvind (http://tech.heliconia.in)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2015 Tecnativa - Pedro M. Baeza (https://www.tecnativa.com)
+# © 2015 AvanzOsc (http://www.avanzosc.es)
+# © 2016 Atul Arvind (http://tech.heliconia.in)
+
 
 {
     "name": "Sequential Code for Leads / Opportunities",
@@ -27,17 +10,11 @@
     "category": "Customer Relationship Management",
     "author": "OdooMRP team, "
               "AvanzOSC, "
-              "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+              "Tecnativa - Pedro M. Baeza, "
               "Atul Arvind, "
               "Odoo Community Association (OCA)",
     "website": "http://www.odoomrp.com",
     "license": "AGPL-3",
-    "contributors": [
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <ajuaristo@gmail.com>",
-        "Atul Arvind <hello@atularvind.com>"
-    ],
     "depends": [
         "crm",
     ],
