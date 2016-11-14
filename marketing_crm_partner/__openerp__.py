d62a3c7baa9bdf8d0d76e6cf29f717b9e459a02d
cubells <vicent@vcubells.net>
2016-11-14 08:05:36 +0100

    marketing_crm_partner migration

--- marketing_crm_partner/__openerp__.py
+++ marketing_crm_partner/__openerp__.py
@@ -1,18 +1,21 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2016 Tecnativa S.L. - Jairo Llopis
+# © 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Tracking Fields in Partners",
     "summary": "Copy tracking fields from leads to partners",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
     "category": "Customer Relationship Management",
-    "website": "http://www.antiun.com",
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "website": "http://www.tecnativa.com",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
     "depends": [
-        "marketing_crm",
+        "marketing",
+        "crm",
     ],
     "data": [
         "views/res_partner_view.xml",
