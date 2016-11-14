cf79d71246870fe3b93a30c0c79eaa7afe65141a
cubells <vicent@vcubells.net>
2016-11-14 08:05:36 +0100

    marketing_crm_partner migration

--- marketing_crm_partner/models/crm_lead.py
+++ marketing_crm_partner/models/crm_lead.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 Antiun Ingeniería S.L. - Jairo Llopis
+# © 2016 Tecnativa S.L. - Jairo Llopis
+# © 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import api, models
@@ -12,6 +13,6 @@ class CRMLead(models.Model):
     def _lead_create_contact(self, lead, name, is_company, parent_id=False):
         """Populate marketing fields in partner."""
         new_context = {"default_%s" % field: lead[field]
-                       for _key, field in self.tracking_fields()}
+                       for _key, field, cookie in self.tracking_fields()}
         return (super(CRMLead, self.with_context(**new_context))
                 ._lead_create_contact(lead, name, is_company, parent_id))
