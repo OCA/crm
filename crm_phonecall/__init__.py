6c764daa904347d68b98378461273e594afd059c
cubells <vicent@vcubells.net>
2017-01-24 06:13:56 +0100

    [9.0][ADD][WIP] crm_phonecall module

---
+++ crm_phonecall/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in root directory
+##############################################################################
+
+from . import models
+from . import wizard
+from . import report
---
+++ crm_phonecall/__openerp__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "CRM Phone Calls",
+    "version": "9.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "author": "Tecnativa, "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.tecnativa.com",
+    "license": "AGPL-3",
+    "contributors": [
+        "Vicent Cubells <vicent.cubells@tecnativa.com>",
+    ],
+    "depends": [
+        'crm',
+        'calendar',
+    ],
+    "data": [
+        'security/crm_security.xml',
+        'wizard/crm_phonecall_to_phonecall_view.xml',
+        'views/crm_phonecall_view.xml',
+        'views/res_partner_view.xml',
+        'report/crm_phonecall_report_view.xml',
+    ],
+    'installable': True,
+}
