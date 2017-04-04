15bb7e29bb9d33fd11e4cfa99ca15ce3d26830ef
cubells <vicent@vcubells.net>
2017-04-05 00:55:11 +0200

    [MIG] crm_phone_call: Migrated to 10.0 (#138)
    * crm_phonecall module (#131)
    
    This module is the part extracted from Odoo v8 crm module from Odoo that allows to record phone calls. As it has been stripped off from Odoo v9, we add again the feature via this module.
    
    Migration scripts are also included to upgrade a v8 database via OpenUpgrade and don't lose your history.
    
    * [MIG] crm_phone_call: Migrated to 10.0
    
     - FIX: Correct some lint errors
    
    * [MIG] crm_phone_call: Migrated to 10.0

---
+++ crm_phonecall/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Tecnativa - Vicent Cubells
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import crm_phonecall
+from . import calendar
+from . import res_partner
