c020e4c7a7a95d6a74ce9d37e7ffdd6d4129bb83
cubells <vicent@vcubells.net>
2017-05-02 18:11:53 +0200

    crm_action module

--- crm_action/models/crm_lead.py
+++ crm_action/models/crm_lead.py
@@ -1,15 +1,43 @@
 # -*- coding: utf-8 -*-
-# © 2015-2016 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# Copyright 2015-2016 Savoir-faire Linux (<http://www.savoirfairelinux.com>)
+# Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from openerp import api, fields, models
 
 
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-    def count_actions(self):
+    def compute_count_actions(self):
         self.actions_count = len(self.action_ids)
+
+    actions_count = fields.Integer(
+        compute='compute_count_actions',
+    )
+    action_ids = fields.One2many(
+        comodel_name='crm.action',
+        inverse_name='lead_id',
+        string='Actions',
+    )
+    # replace native fields "date_action" (Next Action Date)
+    # and "title_action" (Next Action) by related fields
+    date_action = fields.Date(
+        related='next_action_id.date',
+        readonly=True,
+        store=True,
+    )
+    title_action = fields.Char(
+        related='next_action_id.display_name',
+        readonly=True,
+        store=True,
+    )
+    next_action_id = fields.Many2one(
+        comodel_name='crm.action',
+        string='Next Action',
+        compute='compute_next_action',
+        readonly=True, store=True,
+    )
 
     @api.multi
     @api.depends(
@@ -27,20 +55,6 @@ class CrmLead(models.Model):
     @api.multi
     def next_action_done(self):
         self.ensure_one()
-        lead = self[0]
-        if lead.next_action_id:
-            lead.next_action_id.button_confirm()
+        if self.next_action_id:
+            self.next_action_id.button_confirm()
         return True
-
-    actions_count = fields.Integer(compute='count_actions')
-    action_ids = fields.One2many(
-        'crm.action', 'lead_id', string='Actions')
-    # replace native fields "date_action" (Next Action Date)
-    # and "title_action" (Next Action) by related fields
-    date_action = fields.Date(
-        related='next_action_id.date', readonly=True, store=True)
-    title_action = fields.Char(
-        related='next_action_id.display_name', store=True, readonly=True)
-    next_action_id = fields.Many2one(
-        'crm.action', string='Next Action',
-        compute='compute_next_action', readonly=True, store=True)
