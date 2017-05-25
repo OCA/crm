80cae56f6f8c314b222ad1bcc3693ab0c90ff521
bima <bimajatiwijaya@gmail.com>
2017-05-25 18:09:16 +0700

    No module here depend on crm_claim_code and crm_claim_type - Module crm_claim is deleted on odoo 10.0 - Both modules will move to OCA/rma because all needed object from crm_claim odoo9 will move to crm_claim_rma which located on repo OCA/rma

--- crm_claim_type/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2015 Vauxoo: Yanina Aular <yani@vauxoo.com>, Osval Reyes <osval@vauxoo.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import models
