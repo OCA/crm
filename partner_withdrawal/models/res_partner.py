7ce86bf9882052afdaba5972e156218872137618
Luis M. Ontalba <luismaront@gmail.com>
2017-06-12 17:23:11 +0200

    [MIG] partner_withdrawal: Migrated to 10.0

--- partner_withdrawal/models/res_partner.py
+++ partner_withdrawal/models/res_partner.py
@@ -1,28 +1,8 @@
-# -*- coding: utf-8 -*-
-# Python source code encoding : https://www.python.org/dev/peps/pep-0263/
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright :
-#        (c) 2015 Antiun Ingenieria, SL (Madrid, Spain, http://www.antiun.com)
-#                 Antonio Espinosa <antonioea@antiun.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- encoding: utf-8 -*-
+# Copyright 2015 - Antonio Espinosa - Antiun Ingeniería
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from openerp import models, fields, api
+from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
