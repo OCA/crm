f86fd555a9075a246132ac562c44e75a144700c0
mreficent <miquel.raich@eficent.com>
2017-06-26 16:57:36 +0200

    [MIG] crm_sale_marketing: Migration to 10.0

--- crm_sale_marketing/__manifest__.py
+++ crm_sale_marketing/__manifest__.py
@@ -7,7 +7,7 @@
 {
     "name": "Sales Marketing",
     "summary": "Marketing Details of Sales",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
