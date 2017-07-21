60580e10660d8e3a599a4dc7690c98813823f717
mreficent <miquel.raich@eficent.com>
2017-07-21 10:59:10 +0200

    [9.0][MIG] crm_helpdesk: Migration to 9.0

--- crm_helpdesk/models/crm_helpdesk.py
+++ crm_helpdesk/models/crm_helpdesk.py
@@ -1,33 +1,13 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2004-2010 Tiny SPRL (<http://tiny.be>)
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import openerp
-from openerp.addons.crm import crm
-from openerp.osv import fields, osv
-from openerp import tools
-from openerp.tools.translate import _
-from openerp.tools import html2plaintext
+from openerp import api, fields, models, _
+from openerp.tools.mail import html2plaintext
 
 
-class crm_helpdesk(osv.osv):
+class CrmHelpdesk(models.Model):
     """ Helpdesk Cases """
 
     _name = "crm.helpdesk"
@@ -35,98 +15,84 @@ class crm_helpdesk(osv.osv):
     _order = "id desc"
     _inherit = ['mail.thread']
 
-    _columns = {
-            'id': fields.integer('ID', readonly=True),
-            'name': fields.char('Name', required=True),
-            'active': fields.boolean('Active', required=False),
-            'date_action_last': fields.datetime('Last Action', readonly=1),
-            'date_action_next': fields.datetime('Next Action', readonly=1),
-            'description': fields.text('Description'),
-            'create_date': fields.datetime('Creation Date' , readonly=True),
-            'write_date': fields.datetime('Update Date' , readonly=True),
-            'date_deadline': fields.date('Deadline'),
-            'user_id': fields.many2one('res.users', 'Responsible'),
-            'section_id': fields.many2one('crm.case.section', 'Sales Team', \
-                            select=True, help='Responsible sales team. Define Responsible user and Email account for mail gateway.'),
-            'company_id': fields.many2one('res.company', 'Company'),
-            'date_closed': fields.datetime('Closed', readonly=True),
-            'partner_id': fields.many2one('res.partner', 'Partner'),
-            'email_cc': fields.text('Watchers Emails', size=252 , help="These email addresses will be added to the CC field of all inbound and outbound emails for this record before being sent. Separate multiple email addresses with a comma"),
-            'email_from': fields.char('Email', size=128, help="Destination email for email gateway"),
-            'date': fields.datetime('Date'),
-            'ref': fields.reference('Reference', selection=openerp.addons.base.res.res_request.referencable_models),
-            'ref2': fields.reference('Reference 2', selection=openerp.addons.base.res.res_request.referencable_models),
-            'channel_id': fields.many2one('crm.tracking.medium', 'Channel', help="Communication channel."),
-            'planned_revenue': fields.float('Planned Revenue'),
-            'planned_cost': fields.float('Planned Costs'),
-            'priority': fields.selection([('0','Low'), ('1','Normal'), ('2','High')], 'Priority'),
-            'probability': fields.float('Probability (%)'),
-            'categ_id': fields.many2one('crm.case.categ', 'Category', \
-                            domain="['|',('section_id','=',False),('section_id','=',section_id),\
-                            ('object_id.model', '=', 'crm.helpdesk')]"),
-            'duration': fields.float('Duration', states={'done': [('readonly', True)]}),
-            'state': fields.selection(
-                [('draft', 'New'),
-                 ('open', 'In Progress'),
-                 ('pending', 'Pending'),
-                 ('done', 'Closed'),
-                 ('cancel', 'Cancelled')], 'Status', readonly=True, track_visibility='onchange',
-                                  help='The status is set to \'Draft\', when a case is created.\
-                                  \nIf the case is in progress the status is set to \'Open\'.\
-                                  \nWhen the case is over, the status is set to \'Done\'.\
-                                  \nIf the case needs to be reviewed then the status is set to \'Pending\'.'),
-    }
+    name = fields.Char('Name', required=True, index=True,)
+    active = fields.Boolean('Active', required=False, default=True)
+    date_action_last = fields.Datetime('Last Action', readonly=1)
+    date_action_next = fields.Datetime('Next Action', readonly=1)
+    description = fields.Text('Description')
+    create_date = fields.Datetime('Creation Date', readonly=True)
+    write_date = fields.Datetime('Update Date', readonly=True)
+    date_deadline = fields.Date('Deadline')
+    user_id = fields.Many2one(
+        'res.users', string='Responsible', index=True,
+        default=lambda self: self.env.user)
+    team_id = fields.Many2one(
+        'crm.team', string='Sales Team', index=True,
+        help='Responsible sales team. Define Responsible user and '
+             'Email account for mail gateway.')
+    company_id = fields.Many2one(
+        'res.company', string='Company', index=True,
+        default=lambda self: self.env.user.company_id.id)
+    date_open = fields.Datetime('Open', readonly=True)
+    date_closed = fields.Datetime('Closed', readonly=True)
+    partner_id = fields.Many2one('res.partner', string='Partner', index=True)
+    email_cc = fields.Text('Watchers Emails', size=252,
+                           help="These email addresses will be added to the "
+                                "CC field of all inbound and outbound emails"
+                                " for this record before being sent. Separate"
+                                " multiple email addresses with a comma")
+    contact_name = fields.Char('Contact Name', size=64)
+    email_from = fields.Char('Email', size=128, index=True,
+                             help="Destination email for email gateway")
+    date = fields.Datetime('Date', index=True,
+                           default=fields.Datetime.now)
+    channel_id = fields.Many2one('utm.medium', string='Channel',
+                                 help="Communication channel.")
+    planned_revenue = fields.Float('Planned Revenue')
+    priority = fields.Selection([('0', 'Low'), ('1', 'Normal'), ('2', 'High')],
+                                string='Priority', index=True, default='1')
+    probability = fields.Float('Probability (%)')
+    tag_ids = fields.Many2one(
+        'crm.lead.tag', string='Tags',
+        domain="['|',('team_id','=',False),('team_id','=', team_id)]")
+    duration = fields.Float('Duration', states={'done': [('readonly', True)]})
+    lost_reason = fields.Many2one('crm.lost.reason', 'Lost Reason',
+                                  index=True, track_visibility='onchange')
+    state = fields.Selection(
+        [('draft', 'New'),
+         ('open', 'In Progress'),
+         ('pending', 'Pending'),
+         ('done', 'Closed'),
+         ('cancel', 'Cancelled')], 'Status', readonly=True,
+        track_visibility='onchange', index=True,
+        help='The status is set to \'Draft\', when a case is created.\
+              \nIf the case is in progress the status is set to \'Open\'.\
+              \nWhen the case is over, the status is set to \'Done\'.\
+              \nIf the case needs to be reviewed then the status is set to'
+             ' \'Pending\'.', default='draft')
 
-    _defaults = {
-        'active': lambda *a: 1,
-        'user_id': lambda s, cr, uid, c: uid,
-        'state': lambda *a: 'draft',
-        'date': fields.datetime.now,
-        'company_id': lambda s, cr, uid, c: s.pool.get('res.company')._company_default_get(cr, uid, 'crm.helpdesk', context=c),
-        'priority': '1',
-    }
+    @api.onchange('partner_id')
+    def _onchange_partner_id(self):
+        self.email_from = self.partner_id.email
 
-    def on_change_partner_id(self, cr, uid, ids, partner_id, context=None):
-        values = {}
-        if partner_id:
-            partner = self.pool.get('res.partner').browse(cr, uid, partner_id, context=context)
-            values = {
-                'email_from': partner.email,
-            }
-        return {'value': values}
-
-    def write(self, cr, uid, ids, values, context=None):
+    @api.multi
+    def write(self, values):
         """ Override to add case management: open/close dates """
         if values.get('state'):
-            if values.get('state') in ['draft', 'open'] and not values.get('date_open'):
-                values['date_open'] = fields.datetime.now()
-            elif values.get('state') == 'done' and not values.get('date_closed'):
-                values['date_closed'] = fields.datetime.now()
-        return super(crm_helpdesk, self).write(cr, uid, ids, values, context=context)
-
-    def case_escalate(self, cr, uid, ids, context=None):
-        """ Escalates case to parent level """
-        data = {'active': True}
-        for case in self.browse(cr, uid, ids, context=context):
-            if case.section_id and case.section_id.parent_id:
-                parent_id = case.section_id.parent_id
-                data['section_id'] = parent_id.id
-                if parent_id.change_responsible and parent_id.user_id:
-                    data['user_id'] = parent_id.user_id.id
-            else:
-                raise osv.except_osv(_('Error!'), _('You can not escalate, you are already at the top level regarding your sales-team category.'))
-            self.write(cr, uid, [case.id], data, context=context)
-        return True
+            if values.get('state') in ['draft', 'open'] and not values.get(
+                    'date_open'):
+                values['date_open'] = fields.Datetime.now()
+            elif values.get('state') == 'done' and not values.get(
+                    'date_closed'):
+                values['date_closed'] = fields.Datetime.now()
+        return super(CrmHelpdesk, self).write(values)
 
     # -------------------------------------------------------
     # Mail gateway
     # -------------------------------------------------------
 
-    def message_new(self, cr, uid, msg, custom_values=None, context=None):
-        """ Overrides mail_thread message_new that is called by the mailgateway
-            through message_process.
-            This override updates the document according to the email.
-        """
+    @api.model
+    def _prepare_message_new_custom_values(self, msg, custom_values=None):
         if custom_values is None:
             custom_values = {}
         desc = html2plaintext(msg.get('body')) if msg.get('body') else ''
@@ -139,6 +105,15 @@ class crm_helpdesk(osv.osv):
             'partner_id': msg.get('author_id', False),
         }
         defaults.update(custom_values)
-        return super(crm_helpdesk, self).message_new(cr, uid, msg, custom_values=defaults, context=context)
+        return defaults, msg
 
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+    @api.model
+    def message_new(self, msg, custom_values=None):
+        """ Overrides mail_thread message_new that is called by the
+            mailgateway through message_process.
+            This override updates the document according to the email.
+        """
+        custom_values, msg = self._prepare_message_new_custom_values(
+            msg, custom_values)
+        return super(CrmHelpdesk, self).message_new(
+            msg, custom_values=custom_values)
