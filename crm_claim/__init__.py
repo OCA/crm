4b341b95cbefaaf7f3cd770a3a67d0c6589ab209
cubells <vicent@vcubells.net>
2017-09-05 13:48:07 +0200

    [MIG] crm_claim: Migration to 10.0

---
+++ crm_claim/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 Odoo S.A.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import models
+from . import report
---
+++ crm_claim/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 Odoo S.A.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import crm_claim
+from . import crm_claim_category
+from . import crm_claim_stage
+from . import res_partner
