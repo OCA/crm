d8e484a15c0d138b03eab62eceb355c39765912f
David Vidal <chienandalu@gmail.com>
2017-09-21 11:59:20 +0200

    [MIG] marketing_crm_partner: Migration to 10.0

--- marketing_crm_partner/models/crm_lead.py
+++ marketing_crm_partner/models/crm_lead.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 -*-
-# © 2016 Tecnativa S.L. - Jairo Llopis
-# © 2016 Tecnativa S.L. - Vicent Cubells
+# Copyright 2016 Tecnativa S.L. - Jairo Llopis
+# Copyright 2016 Tecnativa S.L. - Vicent Cubells
+# Copyright 2016 Tecnativa S.L. - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, models
+from odoo import api, models
 
 
 class CRMLead(models.Model):
     _inherit = "crm.lead"
 
     @api.model
-    def _lead_create_contact(self, lead, name, is_company, parent_id=False):
+    def _lead_create_contact(self, name, is_company, parent_id=False):
         """Populate marketing fields in partner."""
-        new_context = {"default_%s" % field: lead[field]
-                       for _key, field, cookie in self.tracking_fields()}
-        return (super(CRMLead, self.with_context(**new_context))
-                ._lead_create_contact(lead, name, is_company, parent_id))
+        vals = {
+            field: self[field].id
+            for _key, field, cookie in self.tracking_fields()}
+        partner = super(
+            CRMLead, self)._lead_create_contact(name, is_company, parent_id)
+        partner.write(vals)
+        return partner
