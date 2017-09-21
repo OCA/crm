80ce6f0e2cb17a5de182617637618ba6c7b1eebb
David Vidal <chienandalu@gmail.com>
2017-09-21 11:59:20 +0200

    [MIG] marketing_crm_partner: Migration to 10.0

--- marketing_crm_partner/models/res_partner.py
+++ marketing_crm_partner/models/res_partner.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2016 Tecnativa S.L. - Jairo Llopis
-# © 2016 Tecnativa S.L. - Vicent Cubells
+# Copyright 2016 Tecnativa S.L. - Jairo Llopis
+# Copyright 2016 Tecnativa S.L. - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models
+from odoo import models
 
 
 class ResPartner(models.Model):
