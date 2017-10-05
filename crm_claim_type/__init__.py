e4b31877f6a25d52e736a047f5033f8e5875b786
Bhavesh Odedra <bodedra@ursainfosystems.com>
2017-10-05 11:47:03 +0530

    [MIG] crm_claim_type: Migration to 10.0

--- crm_claim_type/__init__.py
+++ crm_claim_type/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # © 2015 Vauxoo: Yanina Aular <yani@vauxoo.com>, Osval Reyes <osval@vauxoo.com>
+# © 2017 Bhavesh Odedra <bodedra@ursainfosystems.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
