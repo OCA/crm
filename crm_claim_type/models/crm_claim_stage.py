46a927fab7e5a2040e477b5fef0a11c09cfad0aa
Bhavesh Odedra <bodedra@ursainfosystems.com>
2017-10-05 11:47:03 +0530

    [MIG] crm_claim_type: Migration to 10.0

--- crm_claim_type/models/crm_claim_stage.py
+++ crm_claim_type/models/crm_claim_stage.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
+# © 2017 URSA Information Systems
 # © 2015 Vauxoo
 # © 2013 Camptocamp
 # © 2009-2013 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class CrmClaimStage(models.Model):
