ca5969f279cd2150ced1b1a7a1c1b7665f564d63
jesusVMayor <jesus@comunitea.com>
2017-11-07 18:53:17 +0100

    crm_phonecall: Fix action to redirect to the new phonecall when its created from phonecall2phonecall wizard.

--- crm_phonecall/__manifest__.py
+++ crm_phonecall/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "CRM Phone Calls",
-    "version": "10.0.1.1.0",
+    "version": "10.0.1.1.1",
     "category": "Customer Relationship Management",
     "author": "Odoo S.A., "
               "Tecnativa, "
