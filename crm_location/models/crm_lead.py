1503f017155c06d63a4816b508bc7b28ef456148
hprasetyou <hprasetyou@gmail.com>
2018-04-30 10:36:44 +0700

    [MOD] remove # -*- coding: utf-8 -*- line

--- crm_location/models/crm_lead.py
+++ crm_location/models/crm_lead.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antiun Ingenieria - Endika Iglesias <endikaig@antiun.com>
 # Copyright 2017 Tecnativa - Luis Martínez
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
