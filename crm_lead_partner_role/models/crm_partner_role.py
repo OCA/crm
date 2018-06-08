58e1c9f9b571c23a7d75c1e8613f504039cd860f
Akim Juillerat <akim.juillerat@camptocamp.com>
2018-06-08 18:37:32 +0200

    Create module crm_lead_partner_role

---
+++ crm_lead_partner_role/models/crm_partner_role.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models, fields
+
+
+class CrmPartnerRole(models.Model):
+    _name = 'crm.partner.role'
+    _description = 'Lead partners and roles'
+
+    partner_id = fields.Many2one('res.partner', string='Partner')
+
+    role_id = fields.Many2one('crm.role', string='Role')
+
+    crm_lead_id = fields.Many2one('crm.lead')
