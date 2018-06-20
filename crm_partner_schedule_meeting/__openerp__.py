daa7fc434fc46eb427d453e3572ed579888c2683
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-06-20 13:23:23 +0200

    [9.0] add 'crm_partner_schedule_meeting]

---
+++ crm_partner_schedule_meeting/__openerp__.py
@@ -0,0 +1,18 @@
+# Author: Jordi Ballester Alomar
+# Author: Julien Coux
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "CRM Partner Schedule Meeting",
+    "version": "9.0.1.0.0",
+    "category": "Customer Relationship Management",
+    "author": "Eficent, "              
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/crm",
+    "license": "AGPL-3",
+    "depends": [
+        "crm",
+    ],
+    'installable': True,
+}
