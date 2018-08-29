2cd5ce207531408d471662ecedd4d8c0f8b8a6fd
mreficent <miquel.raich@eficent.com>
2018-08-29 12:25:52 +0200

    [MIG] crm_sale_marketing: Migration to 11.0

--- crm_sale_marketing/__manifest__.py
+++ crm_sale_marketing/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # © 2015 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # © 2015 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    "name": "Sales Marketing",
+    "name": "CRM Sale Marketing",
     "summary": "Marketing Details of Sales",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
