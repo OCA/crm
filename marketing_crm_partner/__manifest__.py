72a3eb4607b22f74a24abae9f2882e640922ad2b
Cris Martín <cristina.marrod@gmail.com>
2018-10-18 10:30:06 +0200

    [11.0][MIG] marketing_crm_partner: Migration to 11.0

--- marketing_crm_partner/__manifest__.py
+++ marketing_crm_partner/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 Tecnativa S.L. - Jairo Llopis
 # Copyright 2016 Tecnativa S.L. - Vicent Cubells
 # Copyright 2017 Tecnativa S.L. - David Vidal
+# Copyright 2018 Tecnativa S.L. - Cristina Martin R.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Tracking Fields in Partners",
     "summary": "Copy tracking fields from leads to partners",
-    "version": "10.0.1.0.0",
-    "category": "Customer Relationship Management",
+    "version": "11.0.1.0.0",
+    "category": "Marketing",
     "website": "https://www.github.com/oca/crm",
     "author": "Tecnativa, "
               "Odoo Community Association (OCA)",
@@ -19,8 +19,5 @@
     ],
     "data": [
         "views/res_partner_view.xml",
-    ],
-    "images": [
-        "images/.png",
     ],
 }
