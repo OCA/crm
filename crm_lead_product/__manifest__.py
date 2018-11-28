9057305fbbdfba13f108e466b576639aec4d095b
Adrià Gil Sorribes <adria.gil@eficent.com>
2018-11-28 09:34:31 +0100

    [12.0][ADD] Add new module crm_lead_product
    add filter for open opportunities in pipeline by product report
    
    [IMP] Extend test coverage

---
+++ crm_lead_product/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright (C) 2017 Eficent Business and IT Consulting Services S.L.
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    'name': 'Lead Line Product',
+    'version': '12.0.1.0.0',
+    'category': 'Customer Relationship Management',
+    'license': 'LGPL-3',
+    'summary': 'Adds a lead line in the lead/opportunity model '
+               'in odoo',
+    'author': "Eficent, Odoo Community Association (OCA)",
+    'website': 'http://www.github.com/OCA/crm',
+    'depends': ['crm', 'product'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/crm_lead_views.xml',
+        'views/crm_lead_line_views.xml',
+        'report/crm_product_report_views.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
