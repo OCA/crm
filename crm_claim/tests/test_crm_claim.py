4a5745c40bf99dd6b204861fdf3ca0241f2fedde
Cristina Martin <cristina.marrod@gmail.com>
2018-11-28 10:33:24 +0100

    [MIG] crm_claim: Migration to v12

--- crm_claim/tests/test_crm_claim.py
+++ crm_claim/tests/test_crm_claim.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Vicent Cubells - <vicent.cubells@tecnativa.com>
+# Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import common
