b5530a25bc5d04f5ae24314799f74e8c80546991
Adrià Gil Sorribes <adria.gil@eficent.com>
2018-11-28 09:34:31 +0100

    [12.0][ADD] Add new module crm_lead_product
    add filter for open opportunities in pipeline by product report
    
    [IMP] Extend test coverage

---
+++ crm_lead_product/models/crm_lead.py
@@ -0,0 +1,35 @@
+# Copyright (C) 2018 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class CrmLead(models.Model):
+    _inherit = 'crm.lead'
+
+    lead_line_ids = fields.One2many(
+        comodel_name='crm.lead.line',
+        inverse_name='lead_id',
+        string='Lead Lines',
+    )
+
+    @api.onchange('lead_line_ids')
+    def _onchange_lead_line_ids(self):
+        planned_revenue = 0
+        for lead_line in self.lead_line_ids:
+            if lead_line.planned_revenue != 0:
+                planned_revenue += lead_line.planned_revenue
+                self.planned_revenue = planned_revenue
+
+    @api.multi
+    def _convert_opportunity_data(self, customer, team_id=False):
+        res = super(CrmLead, self)._convert_opportunity_data(customer, team_id)
+
+        # Update planned_revenue
+        planned_revenue = 0
+        for lead_line in self.lead_line_ids:
+            planned_revenue += lead_line.planned_revenue
+
+        res['planned_revenue'] = planned_revenue
+
+        return res
---
+++ crm_lead_product/readme/CONFIGURE.rst
@@ -0,0 +1,2 @@
+
+To configure this module to be available for the Leads the user needs to activate the Leads option in CRM settings
