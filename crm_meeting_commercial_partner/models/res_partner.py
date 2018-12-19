7107b2e8361925cb23137ebc1cf0533e46c7a7db
aheficent <ahenriquez@eficent.com>
2018-12-19 16:37:10 +0100

    meeting count calculation performance

--- crm_meeting_commercial_partner/models/res_partner.py
+++ crm_meeting_commercial_partner/models/res_partner.py
@@ -8,21 +8,25 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    meeting_count = fields.Integer(compute='_compute_meeting_count')
+    meeting_count = fields.Integer(
+        compute='_compute_meeting_count_commercial_partner')
 
     @api.multi
-    def _compute_meeting_count(self):
-        super()._compute_meeting_count()
+    def _compute_meeting_count_commercial_partner(self):
+        att_model = self.env['calendar.attendee']
         for partner in self:
-            partners = partner + partner.child_ids
-            meetings = self.env['calendar.event'].search(
-                [('partner_ids', 'in', partners.ids)])
-            partner.meeting_count = len(meetings)
+            domain = [('partner_id', 'child_of', partner.id)]
+            attendees = att_model.read_group(
+                domain=domain,
+                fields=['event_id'],
+                groupby=['event_id']
+            )
+            partner.meeting_count = len(attendees)
 
     @api.multi
     def schedule_meeting(self):
         self.ensure_one()
-        res = super(ResPartner, self).schedule_meeting()
-        partners = self + self.child_ids
-        res['context']['search_default_partner_ids'] = partners.ids
-        return res
+        action = super(ResPartner, self).schedule_meeting()
+        action['domain'] = [('partner_ids', 'child_of', self.ids)]
+        action['context'].pop('search_default_partner_ids')
+        return action
