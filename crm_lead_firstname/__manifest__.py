a43176627d70c75bc0cded178bb1d0ae77b1b274
Raf Ven <raf.ven@dynapps.be>
2018-12-26 21:31:41 +0100

    [MIG] crm_lead_firstname: Migration to 12.0

--- crm_lead_firstname/__manifest__.py
+++ crm_lead_firstname/__manifest__.py
@@ -1,13 +1,12 @@
-# -*- coding: utf-8 -*-
 # © 2016 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Firstname and Lastname in Leads",
     "summary": "Specify split names for contacts in leads",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "http://www.github.com/OCA/crm",
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
