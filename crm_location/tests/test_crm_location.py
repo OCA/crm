8a2cfa54c54f186f26ca2dd9a1eb05f3e49fecba
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-05-20 11:35:01 +0200

    [MIG] crm_location: Migration to 12.0

--- crm_location/tests/test_crm_location.py
+++ crm_location/tests/test_crm_location.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Tecnativa - Luis M. Ontalba
+# Copyright 2019 Tecnativa - Alexandre Díaz
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0
 
 from odoo.tests import common
@@ -11,42 +11,42 @@ class TestCrmLocation(common.SavepointCase):
     def setUpClass(cls):
         super(TestCrmLocation, cls).setUpClass()
         cls.country = cls.env['res.country'].create({
-            'name': 'Country test',
+            'name': 'Test country',
         })
         cls.state = cls.env['res.country.state'].create({
             'name': 'Test state',
             'code': 'Test state code',
             'country_id': cls.country.id,
         })
-        cls.location = cls.env['res.better.zip'].create({
-            'name': '12345',
-            'city': 'Test city',
+        cls.city = cls.env['res.city'].create({
+            'name': 'Test city',
             'country_id': cls.country.id,
-            'code': 'Test code',
             'state_id': cls.state.id,
+        })
+        cls.location = cls.env['res.city.zip'].create({
+            'name': '12345',
+            'city_id': cls.city.id
         })
         cls.lead = cls.env['crm.lead'].create({
             'name': 'Test lead',
         })
         cls.partner = cls.env['res.partner'].create({
             'name': 'Test partner name',
+            'state_id': cls.state.id,
+            'country_id': cls.country.id,
+            'city_id': cls.city.id,
         })
 
     def test_on_change_city(self):
-        lead = self.lead
-        location = self.location
-        lead.location_id = location.id
-        lead.on_change_city()
-        self.assertEqual(lead.zip, location.name)
-        self.assertEqual(lead.city, location.city)
-        self.assertEqual(lead.state_id, location.state_id)
-        self.assertEqual(lead.country_id, location.country_id)
+        self.lead.location_id = self.location.id
+        self.lead.on_change_city()
+        self.assertEqual(self.lead.zip, '12345')
+        self.assertEqual(self.lead.city, 'Test city')
+        self.assertEqual(self.lead.state_id.name, 'Test state')
+        self.assertEqual(self.lead.country_id.name, 'Test country')
 
     def test_onchange_partner_id_crm_location(self):
-        lead = self.lead
-        partner = self.partner
-        location = self.location
-        partner.zip_id = location.id
-        lead.partner_id = partner.id
-        lead.onchange_partner_id_crm_location()
-        self.assertEqual(lead.location_id, partner.zip_id)
+        self.partner.zip_id = self.location.id
+        self.lead.partner_id = self.partner.id
+        self.lead.onchange_partner_id_crm_location()
+        self.assertEqual(self.lead.location_id.name, '12345')
