33bd6d9bf44e1c4ce0e1dc3f75aed0105e69a53c
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-06-20 11:31:19 +0200

    [MIG] crm_lead_vat: Migration to 12.0

--- crm_lead_vat/__manifest__.py
+++ crm_lead_vat/__manifest__.py
@@ -1,12 +1,11 @@
-# -*- coding: utf-8 -*-
-# © 2015 Antiun Ingeniería, S.L.
+# Copyright 2015 Antiun Ingeniería, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "VAT in leads",
     "summary": "Add VAT field to leads",
-    "version": "8.0.1.1.1",
+    "version": "12.0.1.0.0",
     "category": "Customer Relationship Management",
-    "website": "http://www.antiun.com",
+    "website": "https://github.com/OCA/crm",
     "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
---
+++ crm_lead_vat/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module was written to extend the functionality of CRM leads to support
+setting the VAT.
