22164beacf7239cbb9244c3805ca7c8eab9c2fef
Oihane Crucelaegui <oihanecruce@gmail.com>
2019-07-09 14:10:22 +0200

    [FIX] crm_phonecall: when accessing from partner's opportunities active_id is not correct

--- crm_phonecall/models/crm_lead.py
+++ crm_phonecall/models/crm_lead.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import api, fields, models
+from odoo.tools.safe_eval import safe_eval
 
 
 class CrmLead(models.Model):
@@ -26,3 +27,18 @@ class CrmLead(models.Model):
             lead.phonecall_count = self.env[
                 'crm.phonecall'].search_count(
                 [('opportunity_id', '=', lead.id)])
+
+    @api.multi
+    def button_open_phonecall(self):
+        self.ensure_one()
+        action = self.env.ref('crm_phonecall.crm_case_categ_phone_incoming0')
+        action_dict = action.read()[0] if action else {}
+        action_dict['context'] = safe_eval(
+            action_dict.get('context', '{}'))
+        action_dict['context'].update({
+            'default_opportunity_id': self.id,
+            'search_default_opportunity_id': self.id,
+            'default_partner_id': self.partner_id.id,
+            'default_duration': 1.0,
+        })
+        return action_dict
