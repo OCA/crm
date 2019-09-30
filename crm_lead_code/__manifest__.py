5a94a9faadb0e9cecaaebeb9fc2ddcfee6ec4294
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-09-30 15:10:36 +0530

    [WIP][13.0][MIG] crm_lead_code

--- crm_lead_code/__manifest__.py
+++ crm_lead_code/__manifest__.py
@@ -21,25 +21,19 @@
 
 {
     "name": "Sequential Code for Leads / Opportunities",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "category": "Customer Relationship Management",
     "author": "Tecnativa, "
               "AvanzOSC, "
               "Odoo Community Association (OCA)",
-    "website": "http://www.odoomrp.com",
+    "website": "https://github.com/OCA/crm",
     "license": "AGPL-3",
-    "contributors": [
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
-        "Ana Juaristi <ajuaristo@gmail.com>",
-        "Mathias Markl <mathias.markl@mukit.at>",
-    ],
     "depends": [
         "crm",
     ],
     "data": [
-        "views/crm_lead_view.xml",
         "data/lead_sequence.xml",
+        "views/crm_lead_view.xml",
     ],
     'installable': True,
     "pre_init_hook": "create_code_equal_to_id",
