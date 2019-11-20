e318384f8d47ff2281335d33b0e19183a0bf9d3e
Ronald Portier <ronald@therp.nl>
2019-11-20 02:23:40 +0100

    [FIX] publication. Module does need partner_noncommercial after all.

--- publication/__manifest__.py
+++ publication/__manifest__.py
@@ -2,24 +2,21 @@
 # Copyright 2014-2019 Therp BV <https://therp.nl>.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
-    'name': 'Publication',
-    'version': '10.0.1.1.0',
-    'category': 'Customer Relationship Management',
-    'author': 'Therp BV, '
-              'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'summary': 'Maintain electronic or print publications for your relations.',
-    'depends': [
-        'contract',
+    "name": "Publication",
+    "version": "10.0.1.1.0",
+    "category": "Customer Relationship Management",
+    "author": "Therp BV, " "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "summary": "Maintain electronic or print publications for your relations.",
+    "depends": ["contract", "partner_noncommercial"],
+    "data": [
+        "data/distribution_list_cron.xml",
+        "views/product_template.xml",
+        "views/distribution_list.xml",
+        "views/res_partner.xml",
+        "views/menu.xml",
+        "security/ir.model.access.csv",
     ],
-    'data': [
-        'data/distribution_list_cron.xml',
-        'views/product_template.xml',
-        'views/distribution_list.xml',
-        'views/res_partner.xml',
-        'views/menu.xml',
-        'security/ir.model.access.csv',
-    ],
-    'auto_install': False,
-    'installable': True,
+    "auto_install": False,
+    "installable": True,
 }
