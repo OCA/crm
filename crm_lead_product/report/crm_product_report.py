6fcc0a8bae3d827c1b55cfd0df7e3458e03fb912
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-08 16:52:35 +0100

    [IMP] crm_lead_product: black, isort

--- crm_lead_product/report/crm_product_report.py
+++ crm_lead_product/report/crm_product_report.py
@@ -3,7 +3,7 @@
 
 from psycopg2.extensions import AsIs
 
-from odoo import fields, models, tools, api
+from odoo import api, fields, models, tools
 
 
 class ActivityReport(models.Model):
@@ -12,40 +12,38 @@ class ActivityReport(models.Model):
     _name = "crm.product.report"
     _auto = False
     _description = "CRM Pipeline by Product Analysis"
-    _rec_name = 'id'
+    _rec_name = "id"
 
-    active = fields.Boolean('Active', readonly=True)
-    campaign_id = fields.Many2one('utm.campaign', 'Campaing', readonly=True)
-    country_id = fields.Many2one('res.country', 'Country', readonly=True)
-    company_id = fields.Many2one('res.company', 'Company', readonly=True)
-    create_date = fields.Datetime('Create Date', readonly=True)
-    date_closed = fields.Datetime('Closed Date', readonly=True)
-    date_conversion = fields.Datetime('Conversion Date', readonly=True)
-    date_deadline = fields.Datetime('Deadline Date', readonly=True)
-    date_open = fields.Datetime('Open Date', readonly=True)
-    lost_reason = fields.Many2one('crm.lost.reason', 'Lost Reason',
-                                  readonly=True)
-    name = fields.Char('Lead Name', readonly=True)
-    partner_id = fields.Many2one('res.partner', 'Partner/Customer',
-                                 readonly=True)
-    partner_name = fields.Char('Contact Name', readonly=True)
-    probability = fields.Float('Probability', group_operator='avg',
-                               readonly=True)
-    stage_id = fields.Many2one('crm.stage', 'Stage', readonly=True)
-    team_id = fields.Many2one('crm.team', 'Sales Team', readonly=True)
+    active = fields.Boolean("Active", readonly=True)
+    campaign_id = fields.Many2one("utm.campaign", "Campaing", readonly=True)
+    country_id = fields.Many2one("res.country", "Country", readonly=True)
+    company_id = fields.Many2one("res.company", "Company", readonly=True)
+    create_date = fields.Datetime("Create Date", readonly=True)
+    date_closed = fields.Datetime("Closed Date", readonly=True)
+    date_conversion = fields.Datetime("Conversion Date", readonly=True)
+    date_deadline = fields.Datetime("Deadline Date", readonly=True)
+    date_open = fields.Datetime("Open Date", readonly=True)
+    lost_reason = fields.Many2one("crm.lost.reason", "Lost Reason", readonly=True)
+    name = fields.Char("Lead Name", readonly=True)
+    partner_id = fields.Many2one("res.partner", "Partner/Customer", readonly=True)
+    partner_name = fields.Char("Contact Name", readonly=True)
+    probability = fields.Float("Probability", group_operator="avg", readonly=True)
+    stage_id = fields.Many2one("crm.stage", "Stage", readonly=True)
+    team_id = fields.Many2one("crm.team", "Sales Team", readonly=True)
     type = fields.Char(
-        string='Type',
-        selection=[('lead', 'Lead'), ('opportunity', 'Opportunity')],
-        help="Type is used to separate Leads and Opportunities")
-    user_id = fields.Many2one('res.users', 'Salesperson', readonly=True)
-    category_id = fields.Many2one('product.category', 'Category',
-                                  readonly=True)
-    expected_revenue = fields.Float('Expected Revenue', readonly=True)
-    planned_revenue = fields.Float('Planned Revenue', readonly=True)
-    product_id = fields.Many2one('product.product', 'Product', readonly=True)
-    product_qty = fields.Integer('Product Quantity', readonly=True)
-    product_tmpl_id = fields.Many2one('product.template', 'Product Template',
-                                      readonly=True)
+        string="Type",
+        selection=[("lead", "Lead"), ("opportunity", "Opportunity")],
+        help="Type is used to separate Leads and Opportunities",
+    )
+    user_id = fields.Many2one("res.users", "Salesperson", readonly=True)
+    category_id = fields.Many2one("product.category", "Category", readonly=True)
+    expected_revenue = fields.Float("Expected Revenue", readonly=True)
+    planned_revenue = fields.Float("Planned Revenue", readonly=True)
+    product_id = fields.Many2one("product.product", "Product", readonly=True)
+    product_qty = fields.Integer("Product Quantity", readonly=True)
+    product_tmpl_id = fields.Many2one(
+        "product.template", "Product Template", readonly=True
+    )
 
     def _select(self):
         return """
@@ -91,12 +89,18 @@ class ActivityReport(models.Model):
     @api.model_cr
     def init(self):
         tools.drop_view_if_exists(self._cr, self._table)
-        self._cr.execute("""
+        self._cr.execute(
+            """
             CREATE OR REPLACE VIEW %s AS (
                 %s
                 %s
                 %s
             )
-        """, (AsIs(self._table), AsIs(self._select()),
-              AsIs(self._from()), AsIs(self._join()))
+        """,
+            (
+                AsIs(self._table),
+                AsIs(self._select()),
+                AsIs(self._from()),
+                AsIs(self._join()),
+            ),
         )
