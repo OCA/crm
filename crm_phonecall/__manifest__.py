cb6d3cfa385447378f095d642a923fab0aa6484d
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2020-02-11 12:40:10 +0100

    [IMP] crm_phonecall: black, isort

--- crm_phonecall/__manifest__.py
+++ crm_phonecall/__manifest__.py
@@ -3,25 +3,20 @@
 
 {
     "name": "CRM Phone Calls",
-    "version": "12.0.1.4.0",
+    "version": "13.0.1.0.0",
     "category": "Customer Relationship Management",
-    "author": "Odoo S.A., "
-              "Tecnativa, "
-              "Odoo Community Association (OCA)",
+    "author": "Odoo S.A., Tecnativa, Odoo Community Association (OCA)",
     "website": "https://www.tecnativa.com",
     "license": "AGPL-3",
-    "depends": [
-        'crm',
-        'calendar',
-    ],
+    "depends": ["crm", "calendar"],
     "data": [
-        'security/crm_security.xml',
-        'security/ir.model.access.csv',
-        'wizard/crm_phonecall_to_phonecall_view.xml',
-        'views/crm_phonecall_view.xml',
-        'views/res_partner_view.xml',
-        'views/crm_lead_view.xml',
-        'report/crm_phonecall_report_view.xml',
+        "security/crm_security.xml",
+        "security/ir.model.access.csv",
+        "wizard/crm_phonecall_to_phonecall_view.xml",
+        "views/crm_phonecall_view.xml",
+        "views/res_partner_view.xml",
+        "views/crm_lead_view.xml",
+        "report/crm_phonecall_report_view.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
