6b308bb9d44620c3cbc5f936829c96ca80170355
ludolelarge <52041088+ludolelarge@users.noreply.github.com>
2020-02-19 11:32:15 +0100

    Update res_partner.py

--- crm_claim/models/res_partner.py
+++ crm_claim/models/res_partner.py
@@ -17,9 +17,9 @@ class ResPartner(models.Model):
         partner_data = self.env["crm.claim"].read_group(
             [("partner_id", "in", partners.ids)], ["partner_id"], ["partner_id"]
         )
-        mapped_data = dict(
-            [(m["partner_id"][0], m["partner_id_count"]) for m in partner_data]
-        )
+        mapped_data = {
+            m["partner_id"][0]: m["partner_id_count"] for m in partner_data
+        }
         for partner in self:
             partner.claim_count = mapped_data.get(partner.id, 0)
             for child in partner.child_ids:
