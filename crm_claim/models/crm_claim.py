1ae08647e5cc99144727c1aa63700ab753d504c7
flachica <flachica@gitlab.freyi.es>
2020-03-24 14:42:24 +0100

    FIX crm_claim: Default value for company_id

--- crm_claim/models/crm_claim.py
+++ crm_claim/models/crm_claim.py
@@ -81,7 +81,7 @@ class CrmClaim(models.Model):
     company_id = fields.Many2one(
         comodel_name="res.company",
         string="Company",
-        default=lambda self: self.env.user.company_id,
+        default=lambda self: self.env.company,
     )
     partner_id = fields.Many2one(comodel_name="res.partner", string="Partner")
     email_cc = fields.Text(
