4cc1ebf5ca7b6b5cce59d997bf495e6d4b97078e
flachica <flachica@gitlab.freyi.es>
2020-04-04 12:25:07 +0200

    [IMP] crm_claim: black, isort

--- crm_claim/models/res_partner.py
+++ crm_claim/models/res_partner.py
@@ -6,28 +6,18 @@ from odoo import api, fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    claim_count = fields.Integer(
-        string='# Claims',
-        compute='_compute_claim_count',
-    )
-    claim_ids = fields.One2many(
-        comodel_name='crm.claim',
-        inverse_name='partner_id',
-    )
+    claim_count = fields.Integer(string="# Claims", compute="_compute_claim_count")
+    claim_ids = fields.One2many(comodel_name="crm.claim", inverse_name="partner_id")
 
-    @api.depends('claim_ids', 'child_ids', 'child_ids.claim_ids')
+    @api.depends("claim_ids", "child_ids", "child_ids.claim_ids")
     def _compute_claim_count(self):
-        partners = self | self.mapped('child_ids')
-        partner_data = self.env['crm.claim'].read_group(
-            [('partner_id', 'in', partners.ids)],
-            ['partner_id'],
-            ['partner_id'],
+        partners = self | self.mapped("child_ids")
+        partner_data = self.env["crm.claim"].read_group(
+            [("partner_id", "in", partners.ids)], ["partner_id"], ["partner_id"]
         )
-        mapped_data = dict(
-            [(m['partner_id'][0], m['partner_id_count']) for m in partner_data]
-        )
+        mapped_data = {m["partner_id"][0]: m["partner_id_count"] for m in partner_data}
         for partner in self:
             partner.claim_count = mapped_data.get(partner.id, 0)
             for child in partner.child_ids:
