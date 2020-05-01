e1ef39d2786ccae120d6cb0d096ec815f4b01f59
Stephan Keller <MiStK@gmx.de>
2020-05-01 18:28:06 +0200

    [IMP] crm_stage_type: black, isort, prettier

--- crm_stage_type/models/crm_stage.py
+++ crm_stage_type/models/crm_stage.py
@@ -5,11 +5,15 @@ from odoo import fields, models
 
 
 class Stage(models.Model):
-    _inherit = 'crm.stage'
+    _inherit = "crm.stage"
 
     lead_type = fields.Selection(
-        [('lead', 'Lead'), ('opportunity', 'Opportunity'), ('both', 'Both')],
-        string='Type', required=True, default='both', oldname='type',
+        [("lead", "Lead"), ("opportunity", "Opportunity"), ("both", "Both")],
+        string="Type",
+        required=True,
+        default="both",
+        oldname="type",
         help="This field is used to distinguish stages related to Leads"
-             "from stages related to Opportunities, or to specify stages"
-             "available for both types.")
+        "from stages related to Opportunities, or to specify stages"
+        "available for both types.",
+    )
