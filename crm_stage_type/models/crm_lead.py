90c69cbd3df1072a60eda685a1886bd7088b7ce4
Stephan Keller <MiStK@gmx.de>
2020-05-01 18:28:06 +0200

    [IMP] crm_stage_type: black, isort, prettier

--- crm_stage_type/models/crm_lead.py
+++ crm_stage_type/models/crm_lead.py
@@ -1,36 +1,38 @@
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api, fields, models
 
 
 class Lead(models.Model):
-    _inherit = 'crm.lead'
+    _inherit = "crm.lead"
 
     stage_id = fields.Many2one(
         domain="[('team_id', 'in', [team_id, False]), "
-               "('lead_type', 'in', [type, 'both'])]")
+        "('lead_type', 'in', [type, 'both'])]"
+    )
 
     @api.model
     def _read_group_stage_ids(self, stages, domain, order):
-        ctx_type = self.env.context.get('default_type')
+        ctx_type = self.env.context.get("default_type")
         stages = super(Lead, self)._read_group_stage_ids(stages, domain, order)
-        search_domain = [('id', 'in', stages.ids)]
+        search_domain = [("id", "in", stages.ids)]
         if ctx_type:
-            search_domain += [('lead_type', 'in', [ctx_type, 'both'])]
+            search_domain += [("lead_type", "in", [ctx_type, "both"])]
         stage_ids = stages._search(
-            search_domain, order=order, access_rights_uid=SUPERUSER_ID)
+            search_domain, order=order, access_rights_uid=SUPERUSER_ID
+        )
         return stages.browse(stage_ids)
 
-    def _stage_find(self, team_id=False, domain=None, order='sequence'):
-        ctx_type = self.env.context.get('default_type')
-        types = ['both']
+    def _stage_find(self, team_id=False, domain=None, order="sequence"):
+        ctx_type = self.env.context.get("default_type")
+        types = ["both"]
         if ctx_type:
             types += [ctx_type]
         # check whether we should try to add a condition on type
-        avoid_add_type_term = any([
-            term for term in domain if len(term) == 3 if term[0] == 'lead_type'
-        ])
+        avoid_add_type_term = any(
+            [term for term in domain if len(term) == 3 if term[0] == "lead_type"]
+        )
         if avoid_add_type_term:
             return super(Lead, self)._stage_find(team_id, domain, order)
         # collect all team_ids by adding given one,
@@ -43,41 +45,45 @@ class Lead(models.Model):
                 team_ids.add(lead.team_id.id)
         # generate the domain
         if team_ids:
-            search_domain = ['|', ('team_id', '=', False),
-                             ('team_id', 'in', list(team_ids))]
+            search_domain = [
+                "|",
+                ("team_id", "=", False),
+                ("team_id", "in", list(team_ids)),
+            ]
         else:
-            search_domain = [('team_id', '=', False)]
-        search_domain.append(('lead_type', 'in', types))
+            search_domain = [("team_id", "=", False)]
+        search_domain.append(("lead_type", "in", types))
         # AND with the domain in parameter
         if domain:
             search_domain += list(domain)
         # perform search, return the first found
-        return self.env['crm.stage'].search(
-            search_domain, order=order, limit=1)
+        return self.env["crm.stage"].search(search_domain, order=order, limit=1)
 
     @api.multi
     def merge_opportunity(self, user_id=False, team_id=False):
-        opportunities_head = super(Lead, self).merge_opportunity(
-            user_id, team_id)
+        opportunities_head = super(Lead, self).merge_opportunity(user_id, team_id)
         if opportunities_head.team_id:
-            team_stage_ids = self.env['crm.stage'].search(
-                [('team_id', 'in', [opportunities_head.team_id.id, False]),
-                 ('lead_type', 'in', [opportunities_head.type, 'both'])],
-                order='sequence')
+            team_stage_ids = self.env["crm.stage"].search(
+                [
+                    ("team_id", "in", [opportunities_head.team_id.id, False]),
+                    ("lead_type", "in", [opportunities_head.type, "both"]),
+                ],
+                order="sequence",
+            )
             if opportunities_head.stage_id not in team_stage_ids:
-                opportunities_head.write({
-                    'stage_id': team_stage_ids[0] if team_stage_ids else False
-                })
+                opportunities_head.write(
+                    {"stage_id": team_stage_ids[0] if team_stage_ids else False}
+                )
         return opportunities_head
 
     @api.multi
     def _convert_opportunity_data(self, customer, team_id=False):
         value = super(Lead, self)._convert_opportunity_data(customer, team_id)
-        if not self.stage_id or self.stage_id.lead_type == 'lead':
+        if not self.stage_id or self.stage_id.lead_type == "lead":
             stage = self._stage_find(
-                team_id=team_id, domain=[('lead_type', 'in',
-                                          ['opportunity', 'both'])])
-            value['stage_id'] = stage.id
+                team_id=team_id, domain=[("lead_type", "in", ["opportunity", "both"])]
+            )
+            value["stage_id"] = stage.id
             if stage:
-                value['probability'] = stage.probability
+                value["probability"] = stage.probability
         return value
