a8eaee30791d0cd6c2c70da735081ee4b39bfb16
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-05-05 11:42:33 +0530

    [FIX] Travis
    [UPD] Update crm_lead_code.pot
    
    [UPD] README.rst

--- crm_lead_code/__init__.py
+++ crm_lead_code/__init__.py
@@ -7,19 +7,17 @@ from odoo import api, SUPERUSER_ID
 
 
 def create_code_equal_to_id(cr):
-    cr.execute('ALTER TABLE crm_lead '
-               'ADD COLUMN code character varying;')
-    cr.execute('UPDATE crm_lead '
-               'SET code = id;')
+    cr.execute("ALTER TABLE crm_lead " "ADD COLUMN code character varying;")
+    cr.execute("UPDATE crm_lead " "SET code = id;")
 
 
 def assign_old_sequences(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, dict())
-    lead_obj = env['crm.lead']
-    sequence_obj = env['ir.sequence']
+    lead_obj = env["crm.lead"]
+    sequence_obj = env["ir.sequence"]
     leads = lead_obj.search([], order="id")
     for lead_id in leads.ids:
-        cr.execute('UPDATE crm_lead '
-                   'SET code = %s '
-                   'WHERE id = %s;',
-                   (sequence_obj.next_by_code('crm.lead'), lead_id, ))
+        cr.execute(
+            "UPDATE crm_lead " "SET code = %s " "WHERE id = %s;",
+            (sequence_obj.next_by_code("crm.lead"), lead_id,),
+        )
