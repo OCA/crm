fd59b2db77af53ce4224089a59293b542110dcab
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2020-05-05 11:42:33 +0530

    [FIX] Travis
    [UPD] Update crm_lead_code.pot
    
    [UPD] README.rst

--- crm_lead_code/tests/test_crm_lead_code.py
+++ crm_lead_code/tests/test_crm_lead_code.py
@@ -6,25 +6,22 @@ from odoo.tests.common import TransactionCase
 
 
 class TestCrmLeadCode(TransactionCase):
-
     def setUp(self):
         super(TestCrmLeadCode, self).setUp()
-        self.crm_lead_model = self.env['crm.lead']
-        self.ir_sequence_model = self.env['ir.sequence']
-        self.crm_sequence = self.env.ref('crm_lead_code.sequence_lead')
-        self.crm_lead = self.env.ref('crm.crm_case_1')
+        self.crm_lead_model = self.env["crm.lead"]
+        self.ir_sequence_model = self.env["ir.sequence"]
+        self.crm_sequence = self.env.ref("crm_lead_code.sequence_lead")
+        self.crm_lead = self.env.ref("crm.crm_case_1")
 
     def test_old_lead_code_assign(self):
         crm_leads = self.crm_lead_model.search([])
         for crm_lead in crm_leads:
-            self.assertNotEqual(crm_lead.code, '/')
+            self.assertNotEqual(crm_lead.code, "/")
 
     def test_new_lead_code_assign(self):
         code = self._get_next_code()
-        crm_lead = self.crm_lead_model.create({
-            'name': 'Testing lead code',
-        })
-        self.assertNotEqual(crm_lead.code, '/')
+        crm_lead = self.crm_lead_model.create({"name": "Testing lead code"})
+        self.assertNotEqual(crm_lead.code, "/")
         self.assertEqual(crm_lead.code, code)
 
     def test_copy_lead_code_assign(self):
@@ -34,6 +31,4 @@ class TestCrmLeadCode(TransactionCase):
         self.assertEqual(crm_lead_copy.code, code)
 
     def _get_next_code(self):
-        return self.crm_sequence.get_next_char(
-            self.crm_sequence.number_next_actual
-        )
+        return self.crm_sequence.get_next_char(self.crm_sequence.number_next_actual)
