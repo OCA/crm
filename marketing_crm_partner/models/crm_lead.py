bf1b8ccab353a2352edbf389f4e5512a87b27da3
Stephan Keller <MiStK@gmx.de>
2020-05-12 13:53:14 +0200

    [IMP] marketing_crm_partner: black, isort, prettier

--- marketing_crm_partner/models/crm_lead.py
+++ marketing_crm_partner/models/crm_lead.py
@@ -13,12 +13,10 @@ class CRMLead(models.Model):
     @api.model
     def _create_lead_partner_data(self, name, is_company, parent_id=False):
         """Populate marketing fields in partner."""
-        res = super()._create_lead_partner_data(
-            name, is_company, parent_id=parent_id,
-        )
+        res = super()._create_lead_partner_data(name, is_company, parent_id=parent_id,)
         # We use self.env['utm.mixin'] for not losing possible overrides
         # see https://github.com/odoo/odoo/blob/
         # e5c8071484c883bf78478a39ef2120bcd8f2442d/addons/utm/models/utm.py#L51
-        for _key, field, _cookie in self.env['utm.mixin'].tracking_fields():
+        for _key, field, _cookie in self.env["utm.mixin"].tracking_fields():
             res[field] = self[field].id
         return res
--- marketing_crm_partner/tests/test_lead.py
+++ marketing_crm_partner/tests/test_lead.py
@@ -8,29 +8,24 @@ from odoo.tests.common import TransactionCase
 class LeadCase(TransactionCase):
     def setUp(self):
         super(LeadCase, self).setUp()
-        self.medium = self.env['utm.medium'].create({
-            'name': u'Website'
-        })
-        self.campaign = self.env["utm.campaign"].create({
-            "name": u"Dëmo campaign",
-        })
-        self.source = self.env['utm.source'].create({
-            'name': u'Inteŕnet'
-        })
-        self.lead = self.env["crm.lead"].create({
-            "name": "Lead1",
-            "medium_id": self.medium.id,
-            "campaign_id": self.campaign.id,
-            "source_id": self.source.id,
-        })
+        self.medium = self.env["utm.medium"].create({"name": u"Website"})
+        self.campaign = self.env["utm.campaign"].create({"name": u"Dëmo campaign",})
+        self.source = self.env["utm.source"].create({"name": u"Inteŕnet"})
+        self.lead = self.env["crm.lead"].create(
+            {
+                "name": "Lead1",
+                "medium_id": self.medium.id,
+                "campaign_id": self.campaign.id,
+                "source_id": self.source.id,
+            }
+        )
 
     def test_transfered_values(self):
         """Fields get transfered when creating partner."""
         self.lead.handle_partner_assignation()
         partner_ids = self.lead.handle_partner_assignation()
         for lead_id in partner_ids:
-            self.env["crm.lead"].browse(
-                lead_id).partner_id = partner_ids[lead_id]
-        for _key, field, _cookie in self.env['utm.mixin'].tracking_fields():
+            self.env["crm.lead"].browse(lead_id).partner_id = partner_ids[lead_id]
+        for _key, field, _cookie in self.env["utm.mixin"].tracking_fields():
             self.assertEqual(self.lead[field], self.lead.partner_id[field])
             self.assertIsNot(False, self.lead.partner_id[field])
