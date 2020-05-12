6209c96d2b843458b7289ef846b7fc9a786ad3d3
Stephan Keller <MiStK@gmx.de>
2020-05-12 19:46:06 +0200

    [FIX] make license links secure and remove utf8 encoding comments

--- crm_location_nuts/__manifest__.py
+++ crm_location_nuts/__manifest__.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'NUTS Regions in CRM',
