2ffa460fd1db9881f3fb09febed6ed0501afca1c
Stephan Keller <MiStK@gmx.de>
2020-05-12 19:46:06 +0200

    [FIX] make license links secure and remove utf8 encoding comments

--- crm_claim_type/models/crm_claim.py
+++ crm_claim_type/models/crm_claim.py
@@ -1,7 +1,7 @@
 # Copyright 2015 Vauxoo: Yanina Aular <yani@vauxoo.com>,
 #                        Osval Reyes <osval@vauxoo.com>
 # Copyright 2017 Bhavesh Odedra <bodedra@ursainfosystems.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
--- crm_claim_type/models/crm_claim_stage.py
+++ crm_claim_type/models/crm_claim_stage.py
@@ -2,7 +2,7 @@
 # Copyright 2015 Vauxoo
 # Copyright 2013 Camptocamp
 # Copyright 2009-2013 Akretion
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
 
