5aab1bd616032ee6cc780942656019aab2510284
Stephan Keller <MiStK@gmx.de>
2020-05-12 13:58:32 +0200

    [MIG] marketing_crm_partner: Migration to 13.0

--- marketing_crm_partner/models/crm_lead.py
+++ marketing_crm_partner/models/crm_lead.py
@@ -2,7 +2,7 @@
 # Copyright 2016 Tecnativa S.L. - Vicent Cubells
 # Copyright 2016 Tecnativa S.L. - David Vidal
 # Copyright 2018 Tecnativa S.L. - Cristina Martin R.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
 
@@ -13,7 +13,7 @@ class CRMLead(models.Model):
     @api.model
     def _create_lead_partner_data(self, name, is_company, parent_id=False):
         """Populate marketing fields in partner."""
-        res = super()._create_lead_partner_data(name, is_company, parent_id=parent_id,)
+        res = super()._create_lead_partner_data(name, is_company, parent_id)
         # We use self.env['utm.mixin'] for not losing possible overrides
         # see https://github.com/odoo/odoo/blob/
         # e5c8071484c883bf78478a39ef2120bcd8f2442d/addons/utm/models/utm.py#L51
