4338620bf155ebd5ef9ef04797c232c847926057
Stephan Keller <MiStK@gmx.de>
2020-05-12 19:46:06 +0200

    [FIX] make license links secure and remove utf8 encoding comments

--- crm_location_nuts/models/crm_lead.py
+++ crm_location_nuts/models/crm_lead.py
@@ -1,8 +1,7 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 Antonio Espinosa <antonio.espinosa@tecnativa.com>
 # Copyright 2017 David Vidal <david.vidal@tecnativa.com>
 # Copyright 2019 Alexandre Díaz <alexandre.diaz@tecnativa.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
 
