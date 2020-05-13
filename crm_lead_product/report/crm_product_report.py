0c671aa8622cbd15304cd68bf5a33b19e8edf612
Stephan Keller <MiStK@gmx.de>
2020-05-13 12:21:01 +0200

    [FIX] Clean up license links

--- crm_lead_product/report/crm_product_report.py
+++ crm_lead_product/report/crm_product_report.py
@@ -1,5 +1,5 @@
 # Copyright (C) 2017-19 ForgeFlow S.L. (https://www.forgeflow.com)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from psycopg2.extensions import AsIs
 
