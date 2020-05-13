f5d42d142590aa48d9ef4f49b7f49a4b7c30d126
Stephan Keller <MiStK@gmx.de>
2020-05-13 12:21:01 +0200

    [FIX] Clean up license links

--- crm_lead_firstname/__manifest__.py
+++ crm_lead_firstname/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright 2016 Antiun Ingeniería S.L. - Jairo Llopis
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Firstname and Lastname in Leads",
     "summary": "Specify split names for contacts in leads",
