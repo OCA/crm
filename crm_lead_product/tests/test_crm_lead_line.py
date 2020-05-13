8912515a37539f8c0a667bcb231a96b27b0f0ab5
Stephan Keller <MiStK@gmx.de>
2020-05-13 12:21:01 +0200

    [FIX] Clean up license links

--- crm_lead_product/tests/test_crm_lead_line.py
+++ crm_lead_product/tests/test_crm_lead_line.py
@@ -1,4 +1,4 @@
-# © 2017-19 ForgeFlow S.L. (https://www.forgeflow.com)
+# Copyright 2017-19 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html)
 
 from odoo.tests import common
