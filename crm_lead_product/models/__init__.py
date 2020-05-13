2eb1f1a22bb30dc1c952d9851572bfa5a617f2b4
Stephan Keller <MiStK@gmx.de>
2020-05-13 12:21:01 +0200

    [FIX] Clean up license links

--- crm_lead_product/models/__init__.py
+++ crm_lead_product/models/__init__.py
@@ -1,4 +1,4 @@
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 from . import crm_lead_line
 from . import crm_lead
