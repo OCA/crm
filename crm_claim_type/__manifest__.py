fa2fdc044e2a5c10a586f0438b1052722f477bd3
Manuel Marquez <buzondemam@gmail.com>
2020-06-13 07:15:32 -0400

    [IMP] crm_claim_type: black, isort, prettier

--- crm_claim_type/__manifest__.py
+++ crm_claim_type/__manifest__.py
@@ -3,30 +3,23 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'CRM Claim Types',
-    'category': 'Customer Relationship Management',
-    'summary': 'Claim types for CRM',
-    'author': 'Odoo Community Association (OCA),'
-              'Vauxoo,'
-              'Ursa Information Systems',
-    'website': 'https://github.com/OCA/crm',
-    'license': 'AGPL-3',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'crm_claim',
+    "name": "CRM Claim Types",
+    "category": "Customer Relationship Management",
+    "summary": "Claim types for CRM",
+    "author": "Odoo Community Association (OCA)," "Vauxoo," "Ursa Information Systems",
+    "website": "https://github.com/OCA/crm",
+    "license": "AGPL-3",
+    "version": "13.0.1.0.0",
+    "depends": ["crm_claim"],
+    "data": [
+        "data/crm_claim_type.xml",
+        "data/crm_claim_stage.xml",
+        "security/ir.model.access.csv",
+        "views/crm_claim.xml",
+        "views/crm_claim_stage.xml",
+        "views/crm_claim_type.xml",
     ],
-    'data': [
-        'data/crm_claim_type.xml',
-        'data/crm_claim_stage.xml',
-        'security/ir.model.access.csv',
-        'views/crm_claim.xml',
-        'views/crm_claim_stage.xml',
-        'views/crm_claim_type.xml',
-    ],
-    'demo': [
-        'demo/crm_claim.xml',
-        'demo/crm_claim_stage.xml',
-    ],
-    'installable': True,
-    'auto_install': False,
+    "demo": ["demo/crm_claim.xml", "demo/crm_claim_stage.xml"],
+    "installable": True,
+    "auto_install": False,
 }
