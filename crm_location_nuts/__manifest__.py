7df3bc7880bafb6bd87265cb717609063a97b064
Manuel Marquez <buzondemam@gmail.com>
2020-06-23 18:22:46 -0400

    [IMP] crm_location_nuts: black, isort, prettier.

--- crm_location_nuts/__manifest__.py
+++ crm_location_nuts/__manifest__.py
@@ -3,20 +3,13 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'NUTS Regions in CRM',
-    'category': 'Customer Relationship Management',
-    'version': '12.0.1.0.0',
-    'depends': [
-        'crm',
-        'sales_team',
-        'base_location_nuts',
-    ],
-    'data': [
-        'views/crm_lead_view.xml',
-    ],
-    'author': 'Tecnativa, '
-              'Odoo Community Association (OCA)',
-    'website': 'https://www.tecnativa.com',
-    'license': 'AGPL-3',
-    'installable': True,
+    "name": "NUTS Regions in CRM",
+    "category": "Customer Relationship Management",
+    "version": "13.0.1.0.0",
+    "depends": ["crm", "sales_team", "base_location_nuts"],
+    "data": ["views/crm_lead_view.xml"],
+    "author": "Tecnativa, " "Odoo Community Association (OCA)",
+    "website": "https://www.tecnativa.com",
+    "license": "AGPL-3",
+    "installable": True,
 }
