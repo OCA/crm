5349efcdf1b64ac0742b262d234fb1811614566c
mreficent <miquel.raich@eficent.com>
2020-10-01 12:50:59 +0200

    [UPD] Eficent -> ForgeFlow

--- crm_meeting_commercial_partner/__manifest__.py
+++ crm_meeting_commercial_partner/__manifest__.py
@@ -1,12 +1,12 @@
 # Author: Jordi Ballester Alomar
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018 ForgeFlow, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "CRM Meeting Commercial Partner",
     "version": "13.0.1.0.1",
     "category": "Customer Relationship Management",
-    "author": "Eficent, Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/crm",
     "license": "AGPL-3",
     "depends": ["crm"],
