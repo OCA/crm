c37e22a1d9d1e56992357074a077b9b5d98efcec
RIDA YAHLA <mi_reda2012@live.fr>
2020-11-11 20:21:50 +0100

    [MIG] crm_lead_vat: Migration to 14.0

--- crm_lead_vat/__manifest__.py
+++ crm_lead_vat/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "VAT in leads",
     "summary": "Add VAT field to leads",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/crm",
     "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
