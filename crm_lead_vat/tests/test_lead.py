ed86fdeacb134329fe1a5d7b6bf898f4e734a3f7
RIDA YAHLA <mi_reda2012@live.fr>
2020-11-11 20:21:50 +0100

    [MIG] crm_lead_vat: Migration to 14.0

--- crm_lead_vat/tests/test_lead.py
+++ crm_lead_vat/tests/test_lead.py
@@ -8,7 +8,7 @@ class LeadCase(TransactionCase):
     def setUp(self):
         super(LeadCase, self).setUp()
         self.lead = self.env["crm.lead"].create(
-            {"name": __file__, "partner_name": u"HÎ"}
+            {"name": __file__, "partner_name": "HÎ"}
         )
         self.partner = self.env["res.partner"].create({"name": __file__})
         self.test_field = "ES98765432M"
@@ -16,16 +16,14 @@ class LeadCase(TransactionCase):
     def test_transfered_values(self):
         """Field gets transfered when creating partner."""
         self.lead.vat = self.test_field
-        partner_ids = self.lead.handle_partner_assignation()
-        for lead_id in partner_ids:
-            self.env["crm.lead"].browse(lead_id).partner_id = partner_ids[lead_id]
+        self.lead.handle_partner_assignment()
         self.assertEqual(self.lead.partner_id.vat, self.test_field)
 
     def test_onchange_partner_id(self):
         """Lead gets VAT from partner when linked to it."""
         self.partner.vat = self.test_field
-        result = self.lead._onchange_partner_id_values(self.lead.partner_id.id)
+        result = self.lead._prepare_values_from_partner(self.lead.partner_id)
         self.assertNotIn("vat", result)
         self.lead.partner_id = self.partner
-        result = self.lead._onchange_partner_id_values(self.lead.partner_id.id)
+        result = self.lead._prepare_values_from_partner(self.lead.partner_id)
         self.assertEqual(result["vat"], self.test_field)
