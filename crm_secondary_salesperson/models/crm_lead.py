7fd7690e4634c5bac826f4da945abf82bc36c62c
Marcel Savegnago <marcel.savegnago@gmail.com>
2020-12-31 12:53:08 -0300

    [IMP] check secondary_user_id != user_id

--- crm_secondary_salesperson/models/crm_lead.py
+++ crm_secondary_salesperson/models/crm_lead.py
@@ -12,3 +12,9 @@ class CrmLead(models.Model):
         'res.users',
         string='Secondary Salesperson',
         track_visibility='onchange')
+
+    _sql_constraints = [
+        ('secondary_user_id',
+         'CHECK((secondary_user_id IS NULL) OR (secondary_user_id != user_id))',
+         'The secondary salesperson must be different from the primary salesperson!'),
+    ]
