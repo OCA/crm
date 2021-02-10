825832256fefba37f73df574c139a76d35680d8f
Eder Brito <britoederr@gmail.com>
2021-02-10 00:35:05 -0300

    fixing travis errors

--- crm_automated_activity/tests/test_crm_automated_activity.py
+++ crm_automated_activity/tests/test_crm_automated_activity.py
@@ -15,14 +15,15 @@ class TestAutomatedActivity(common.SavepointCase):
             {"name": "Test Stage", "create_automated_activity": True}
         )
 
-        cls.automated_activity = cls.env["automated.activity"].create(
+        cls.crm_stage.automated_activity_ids(
+            0,
+            0,
             {
-                "crm_stage_id": cls.crm_stage.id,
                 "apply_in": "create",
                 "activity_type_id": cls.activity_type.id,
                 "summary": "Test Automated Activity",
                 "days_deadline": "3",
-            }
+            },
         )
 
         cls.crm_lead = cls.env["crm.lead"].create(
@@ -30,11 +31,7 @@ class TestAutomatedActivity(common.SavepointCase):
         )
 
         cls.mail_activity = cls.env["mail.activity"].search(
-            [
-                ("res_id", "=", cls.crm_lead.id),
-                ("res_model", "=", "crm.lead"),
-                ("res_name", "=", cls.crm_lead.name),
-            ]
+            [("res_id", "=", cls.crm_lead.id), ("res_model", "=", "crm.lead")]
         )
 
     def test_new_automated_activity(self):
@@ -43,10 +40,10 @@ class TestAutomatedActivity(common.SavepointCase):
 
         self.assertTrue(self.crm_stage)
 
-        self.assertTrue(self.automated_activity)
+        self.assertTrue(self.crm_stage.automated_activity)
 
         self.assertTrue(self.crm_lead)
 
         self.assertTrue(self.mail_activity)
 
-        self.assertIn(self.crm_lead.activity_ids.id, self.mail_activity.ids)
+        self.assertIn(self.crm_lead.activity_ids.ids, self.mail_activity.id)
