2add8e72646e714ec285468c2621129548abf8e6
Eder Brito <britoederr@gmail.com>
2021-02-14 04:07:09 -0300

    Correcting tests file

--- crm_automated_activity/tests/test_crm_automated_activity.py
+++ crm_automated_activity/tests/test_crm_automated_activity.py
@@ -11,7 +11,15 @@ class TestAutomatedActivity(common.SavepointCase):
 
         cls.activity_type = cls.env.ref("mail.mail_activity_data_call")
 
-        cls.crm_automated_actitivy = cls.env["crm.automated.activity"].create(
+        cls.activity_type2 = cls.env["mail.activity.type"].create(
+            {"name": "Activity Type Week", "delay_count": 2, "delay_unit": "weeks"}
+        )
+
+        cls.activity_type3 = cls.env["mail.activity.type"].create(
+            {"name": "Activity Type Months", "delay_count": 1, "delay_unit": "months"}
+        )
+
+        cls.crm_automated_acitivy = cls.env["crm.automated.activity"].create(
             {
                 "apply_in": "create_write",
                 "activity_type_id": cls.activity_type.id,
@@ -20,13 +28,19 @@ class TestAutomatedActivity(common.SavepointCase):
             }
         )
 
-        cls.crm_automated_actitivy._onchange_activity_type()
+        cls.crm_automated_acitivy._onchange_activity_type()
+
+        cls.crm_automated_acitivy.activity_type_id = cls.activity_type2.id
+        cls.crm_automated_acitivy._onchange_activity_type()
+
+        cls.crm_automated_acitivy.activity_type_id = cls.activity_type3.id
+        cls.crm_automated_acitivy._onchange_activity_type()
 
         cls.crm_stage = cls.env["crm.stage"].create(
             {
                 "name": "Test Stage",
                 "create_automated_activity": True,
-                "automated_activity_ids": [(4, cls.crm_automated_actitivy.id, 0)],
+                "automated_activity_ids": [(4, cls.crm_automated_acitivy.id, 0)],
             }
         )
 
@@ -34,7 +48,18 @@ class TestAutomatedActivity(common.SavepointCase):
             {
                 "name": "Test Stage 2",
                 "create_automated_activity": True,
-                "automated_activity_ids": [(4, cls.crm_automated_actitivy.id, 0)],
+                "automated_activity_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "apply_in": "create_write",
+                            "activity_type_id": cls.activity_type3.id,
+                            "summary": "Test Automated Activity",
+                            "days_deadline": 3,
+                        },
+                    )
+                ],
             }
         )
 
@@ -50,6 +75,8 @@ class TestAutomatedActivity(common.SavepointCase):
         cls.crm_lead.write({"stage_id": cls.crm_stage2.id})
 
     def test_new_automated_activity(self):
+
+        self.assertTrue(self.activity_type)
 
         self.assertTrue(self.crm_stage)
 
