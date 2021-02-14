7ab86e40eabff94104352c84fbd7e1283e57d134
Eder Brito <britoederr@gmail.com>
2021-02-14 03:09:21 -0300

    Correcting tests file

--- crm_automated_activity/tests/test_crm_automated_activity.py
+++ crm_automated_activity/tests/test_crm_automated_activity.py
@@ -12,21 +12,12 @@ class TestAutomatedActivity(common.SavepointCase):
         cls.activity_type = cls.env.ref("mail.mail_activity_data_call")
 
         cls.activity_type2 = cls.env["mail.activity.type"].create(
-             {
-                 'name': 'Activity Type Week',
-                 'delay_count': 2,
-                 'delay_unit': 'weeks'
-             }
+            {"name": "Activity Type Week", "delay_count": 2, "delay_unit": "weeks"}
         )
 
         cls.activity_type3 = cls.env["mail.activity.type"].create(
-             {
-                 'name': 'Activity Type Months',
-                 'delay_count': 1,
-                 'delay_unit': 'months'
-             }
+            {"name": "Activity Type Months", "delay_count": 1, "delay_unit": "months"}
         )
-
 
         cls.crm_automated_acitivy = cls.env["crm.automated.activity"].create(
             {
@@ -70,7 +61,7 @@ class TestAutomatedActivity(common.SavepointCase):
             }
         )
 
-        cls.crm_lead.write({'stage_id': cls.crm_stage2.id})
+        cls.crm_lead.write({"stage_id": cls.crm_stage2.id})
 
     def test_new_automated_activity(self):
 
