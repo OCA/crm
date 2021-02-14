32ec0a355e9f84678dfffc7c587a2233c747a5a7
Eder Brito <britoederr@gmail.com>
2021-02-14 03:05:34 -0300

    Correcting tests file

--- crm_automated_activity/tests/test_crm_automated_activity.py
+++ crm_automated_activity/tests/test_crm_automated_activity.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Pingo Tecnologia - Éder Brito
+# Copyright 2021 Pingo Tecnologia - Eder Brito
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.tests import common
@@ -11,9 +11,26 @@ class TestAutomatedActivity(common.SavepointCase):
 
         cls.activity_type = cls.env.ref("mail.mail_activity_data_call")
 
+        cls.activity_type2 = cls.env["mail.activity.type"].create(
+             {
+                 'name': 'Activity Type Week',
+                 'delay_count': 2,
+                 'delay_unit': 'weeks'
+             }
+        )
+
+        cls.activity_type3 = cls.env["mail.activity.type"].create(
+             {
+                 'name': 'Activity Type Months',
+                 'delay_count': 1,
+                 'delay_unit': 'months'
+             }
+        )
+
+
         cls.crm_automated_acitivy = cls.env["crm.automated.activity"].create(
             {
-                "apply_in": "create",
+                "apply_in": "create_write",
                 "activity_type_id": cls.activity_type.id,
                 "summary": "Test Automated Activity",
                 "days_deadline": 3,
@@ -22,9 +39,23 @@ class TestAutomatedActivity(common.SavepointCase):
 
         cls.crm_automated_acitivy._onchange_activity_type()
 
+        cls.crm_automated_acitivy.activity_type_id = cls.activity_type2.id
+        cls.crm_automated_acitivy._onchange_activity_type()
+
+        cls.crm_automated_acitivy.activity_type_id = cls.activity_type3.id
+        cls.crm_automated_acitivy._onchange_activity_type()
+
         cls.crm_stage = cls.env["crm.stage"].create(
             {
                 "name": "Test Stage",
+                "create_automated_activity": True,
+                "automated_activity_ids": [(4, cls.crm_automated_acitivy.id, 0)],
+            }
+        )
+
+        cls.crm_stage2 = cls.env["crm.stage"].create(
+            {
+                "name": "Test Stage 2",
                 "create_automated_activity": True,
                 "automated_activity_ids": [(4, cls.crm_automated_acitivy.id, 0)],
             }
@@ -38,6 +69,8 @@ class TestAutomatedActivity(common.SavepointCase):
                 "user_id": cls.env.user.id,
             }
         )
+
+        cls.crm_lead.write({'stage_id': cls.crm_stage2.id})
 
     def test_new_automated_activity(self):
 
