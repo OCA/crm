7104348c05699056984722b1b6e509125e60664e
Eder Brito <britoederr@gmail.com>
2021-02-14 03:40:17 -0300

    Correcting tests file

--- crm_automated_activity/tests/test_crm_automated_activity.py
+++ crm_automated_activity/tests/test_crm_automated_activity.py
@@ -19,7 +19,7 @@ class TestAutomatedActivity(common.SavepointCase):
             {"name": "Activity Type Months", "delay_count": 1, "delay_unit": "months"}
         )
 
-        cls.crm_automated_acitivy = cls.env["crm.automated.activity"].create(
+        cls.crm_automated_actitivy = cls.env["crm.automated.activity"].create(
             {
                 "apply_in": "create_write",
                 "activity_type_id": cls.activity_type.id,
@@ -28,19 +28,19 @@ class TestAutomatedActivity(common.SavepointCase):
             }
         )
 
-        cls.crm_automated_acitivy._onchange_activity_type()
+        cls.crm_automated_actitivy._onchange_activity_type()
 
-        cls.crm_automated_acitivy.activity_type_id = cls.activity_type2.id
-        cls.crm_automated_acitivy._onchange_activity_type()
+        cls.crm_automated_actitivy.activity_type_id = cls.activity_type2.id
+        cls.crm_automated_actitivy._onchange_activity_type()
 
-        cls.crm_automated_acitivy.activity_type_id = cls.activity_type3.id
-        cls.crm_automated_acitivy._onchange_activity_type()
+        cls.crm_automated_actitivy.activity_type_id = cls.activity_type3.id
+        cls.crm_automated_actitivy._onchange_activity_type()
 
         cls.crm_stage = cls.env["crm.stage"].create(
             {
                 "name": "Test Stage",
                 "create_automated_activity": True,
-                "automated_activity_ids": [(4, cls.crm_automated_acitivy.id, 0)],
+                "automated_activity_ids": [(4, cls.crm_automated_actitivy.id, 0)],
             }
         )
 
@@ -48,7 +48,7 @@ class TestAutomatedActivity(common.SavepointCase):
             {
                 "name": "Test Stage 2",
                 "create_automated_activity": True,
-                "automated_activity_ids": [(4, cls.crm_automated_acitivy.id, 0)],
+                "automated_activity_ids": [(4, cls.crm_automated_actitivy.id, 0)],
             }
         )
 
@@ -64,8 +64,6 @@ class TestAutomatedActivity(common.SavepointCase):
         cls.crm_lead.write({"stage_id": cls.crm_stage2.id})
 
     def test_new_automated_activity(self):
-
-        self.assertTrue(self.activity_type)
 
         self.assertTrue(self.crm_stage)
 
