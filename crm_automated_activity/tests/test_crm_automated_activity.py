a5563a534a56a644eab3a289a266af6457899a3a
Eder Brito <britoederr@gmail.com>
2021-02-14 02:17:41 -0300

    correcting tests file

--- crm_automated_activity/tests/test_crm_automated_activity.py
+++ crm_automated_activity/tests/test_crm_automated_activity.py
@@ -11,27 +11,32 @@ class TestAutomatedActivity(common.SavepointCase):
 
         cls.activity_type = cls.env.ref("mail.mail_activity_data_call")
 
+        cls.crm_automated_acitivy = cls.env["crm.automated.activity"].create(
+            {
+                "apply_in": "create",
+                "activity_type_id": cls.activity_type.id,
+                "summary": "Test Automated Activity",
+                "days_deadline": 3,
+            }
+        )
+
+        cls.crm_automated_acitivy._onchange_activity_type()
+
         cls.crm_stage = cls.env["crm.stage"].create(
             {
                 "name": "Test Stage",
                 "create_automated_activity": True,
-                "automated_activity_ids": [
-                    (
-                        0,
-                        0,
-                        {
-                            "apply_in": "create",
-                            "activity_type_id": cls.activity_type.id,
-                            "summary": "Test Automated Activity",
-                            "days_deadline": 3,
-                        },
-                    )
-                ],
+                "automated_activity_ids": [(4, cls.crm_automated_acitivy.id, 0)],
             }
         )
 
         cls.crm_lead = cls.env["crm.lead"].create(
-            {"name": "Test Lead", "type": "opportunity", "user_id": cls.env.user.id}
+            {
+                "name": "Test Lead",
+                "type": "opportunity",
+                "stage_id": cls.crm_stage.id,
+                "user_id": cls.env.user.id,
+            }
         )
 
     def test_new_automated_activity(self):
