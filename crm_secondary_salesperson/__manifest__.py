d15cf05e663b155d4707878c1d839449e9e96002
Víctor Martínez <victor.martinez@tecnativa.com>
2021-03-09 11:06:25 +0100

    [IMP] crm_secondary_salesperson: black, isort, prettier

--- crm_secondary_salesperson/__manifest__.py
+++ crm_secondary_salesperson/__manifest__.py
@@ -2,20 +2,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Crm Secondary Salesperson',
-    'category': 'Customer Relationship Management',
-    'summary': """
+    "name": "Crm Secondary Salesperson",
+    "category": "Customer Relationship Management",
+    "summary": """
         CRM Secondary Salesperson""",
-    'version': '12.0.1.0.1',
-    'license': 'AGPL-3',
-    'author': 'Escodoo, Odoo Community Association (OCA)',
-    'maintainers': ['marcelsavegnago'],
-    'website': 'https://github.com/OCA/crm',
-    'images': ['static/description/banner.png'],
-    'depends': [
-        'crm',
-    ],
-    'data': [
-        'views/crm_lead.xml',
-    ],
+    "version": "12.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "Escodoo, Odoo Community Association (OCA)",
+    "maintainers": ["marcelsavegnago"],
+    "website": "https://github.com/OCA/crm",
+    "images": ["static/description/banner.png"],
+    "depends": ["crm",],
+    "data": ["views/crm_lead.xml",],
 }
--- crm_secondary_salesperson/models/crm_lead.py
+++ crm_secondary_salesperson/models/crm_lead.py
@@ -6,15 +6,16 @@ from odoo import fields, models
 
 class CrmLead(models.Model):
 
-    _inherit = 'crm.lead'
+    _inherit = "crm.lead"
 
     secondary_user_id = fields.Many2one(
-        'res.users',
-        string='Secondary Salesperson',
-        track_visibility='onchange')
+        "res.users", string="Secondary Salesperson", track_visibility="onchange"
+    )
 
     _sql_constraints = [
-        ('secondary_user_id',
-         'CHECK((secondary_user_id IS NULL) OR (secondary_user_id != user_id))',
-         'The secondary salesperson must be different from the primary salesperson!'),
+        (
+            "secondary_user_id",
+            "CHECK((secondary_user_id IS NULL) OR (secondary_user_id != user_id))",
+            "The secondary salesperson must be different from the primary salesperson!",
+        ),
     ]
