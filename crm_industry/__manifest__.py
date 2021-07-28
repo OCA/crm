815660ed6a6ac749f96558c323ceb01c90e2f5b1
ludlaf <ldlafaurie@gmail.com>
2021-07-28 10:34:39 +0200

    [MIG] crm_industry: Migration to 14.0

--- crm_industry/__manifest__.py
+++ crm_industry/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "CRM Industry",
     "summary": "Link leads/opportunities to industries",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/crm",
     "author": "Tecnativa, Odoo Community Association (OCA)",
