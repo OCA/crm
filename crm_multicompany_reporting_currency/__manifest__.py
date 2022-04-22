cc3634f925646fda96776424d6482c4e78970df0
Maksym Yankin <yankinmk@gmail.com>
2022-04-22 12:37:35 +0300

    [15.0] add crm_multicompany_reporting_currency

---
+++ crm_multicompany_reporting_currency/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "CRM Multicompany Reporting Currency",
+    "summary": "Adds Amount in multicompany reporting currency to CRM Lead",
+    "version": "15.0.1.0.1",
+    "category": "Sales",
+    "author": "Camptocamp SA, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["crm", "base_multicompany_reporting_currency"],
+    "website": "https://github.com/OCA/crm",
+    "data": ["views/crm_lead_views.xml"],
+    "installable": True,
+}
