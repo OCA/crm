57f3affc438050becd91300c329f67fa6200051d
Galoperd <jvcsan@gmail.com>
2022-09-03 09:37:38 +0200

    [IMP] crm_lead_firstname: black, isort, prettier

--- crm_lead_firstname/tests/test_crm_lead.py
+++ crm_lead_firstname/tests/test_crm_lead.py
@@ -1,10 +1,10 @@
 # Copyright 2016 Antiun Ingeniería S.L. - Jairo Llopis
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests.common import Form, SavepointCase
+from odoo.tests.common import Form, TransactionCase
 
 
-class FirstNameCase(SavepointCase):
+class FirstNameCase(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(FirstNameCase, cls).setUpClass()
