6d5250fe41cdecbb3d8b6e2708f86d86810d78af
Manuel Marquez <mmarquez@indexacorp.com>
2022-09-12 19:55:28 -0400

    [ADD] crm_industry: Propagate industries from customer contact to lead/opportunity

--- crm_industry/tests/test_crm_lead.py
+++ crm_industry/tests/test_crm_lead.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.exceptions import UserError
+from odoo.tests import Form
 from odoo.tests.common import TransactionCase
 
 
@@ -39,3 +40,29 @@ class TestCrmLead(TransactionCase):
         )
         self.assertEqual(partner.industry_id, lead.industry_id)
         self.assertEqual(partner.secondary_industry_ids, lead.secondary_industry_ids)
+
+    def test_propagate_industries_from_contact(self):
+        res_partner_industry = self.env["res.partner.industry"]
+        industry_1 = res_partner_industry.create({"name": "IT/Communications"})
+        industry_2 = res_partner_industry.create(
+            {"name": "AI/Machine Learning", "parent_id": industry_1.id}
+        )
+        industry_3 = res_partner_industry.create(
+            {"name": "ERP Systems", "parent_id": industry_1.id}
+        )
+        customer = self.env["res.partner"].create(
+            {
+                "name": "Test Customer",
+                "industry_id": industry_1.id,
+                "secondary_industry_ids": [
+                    (4, industry_2.id, 0),
+                    (4, industry_3.id, 0),
+                ],
+            }
+        )
+        lead_form = Form(self.env["crm.lead"])
+        lead_form.name = "Test Lead"
+        lead_form.partner_id = customer
+        lead = lead_form.save()
+        self.assertEqual(lead.industry_id, customer.industry_id)
+        self.assertEqual(lead.secondary_industry_ids, customer.secondary_industry_ids)
