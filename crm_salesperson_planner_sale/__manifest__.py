976b75c60894cd4f00a73c6791bbd2a4662919e9
Víctor Martínez <victor.martinez@tecnativa.com>
2022-11-28 16:08:54 +0100

    [MIG] crm_salesperson_planner_sale: Migration to 15.0
    TT38993

--- crm_salesperson_planner_sale/__manifest__.py
+++ crm_salesperson_planner_sale/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 {
     "name": "Crm Salesperson Planner Sale",
-    "version": "13.0.1.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "category": "Customer Relationship Management",
-    "author": "Sygel Technology," "Odoo Community Association (OCA)",
+    "author": "Sygel Technology, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/crm",
     "license": "AGPL-3",
     "depends": ["crm_salesperson_planner", "sale"],
