271e8196c61b97bee7ebc1cb99c3d56ec8c78cb5
Stefan <stefan.ungureanu@tecnativa.com>
2023-03-20 09:11:19 +0100

    [MIG] crm_lead_vat: Migration to 15.0

--- crm_lead_vat/__manifest__.py
+++ crm_lead_vat/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "VAT in leads",
     "summary": "Add VAT field to leads",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/crm",
     "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
