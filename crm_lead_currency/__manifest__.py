2e2725da509dce54bdabb34a32442d21736e14b9
Jose Sosa <josesl@vauxoo.com>
2023-04-18 17:30:09 -0600

    [MIG] crm_lead_currency: Migration to 14.0

--- crm_lead_currency/__manifest__.py
+++ crm_lead_currency/__manifest__.py
@@ -5,15 +5,18 @@
     "name": "CRM Lead Currency",
     "summary": """
         On leads/opportunities, add the amount in the customer's currency.""",
-    "version": "12.0.1.0.1",
+    "maintainers": [
+        "luisg123v",
+    ],
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
-    "author": "Camptocamp SA,Odoo Community Association (OCA)",
+    "author": "Camptocamp SA,Odoo Community Association (OCA),Vauxoo",
     "website": "https://github.com/OCA/crm",
     "depends": [
         "crm",
     ],
     "data": [
-        "views/crm_lead_opportunity_currency_views.xml",
+        "views/crm_lead_views.xml",
     ],
     "installable": True,
 }
