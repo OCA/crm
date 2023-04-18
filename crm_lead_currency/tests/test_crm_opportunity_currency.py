1266b9b1ca9e722c6346c444386c1c5ea18377aa
Jose Sosa <josesl@vauxoo.com>
2023-04-18 17:30:09 -0600

    [MIG] crm_lead_currency: Migration to 14.0

--- crm_lead_currency/tests/test_crm_opportunity_currency.py
+++ crm_lead_currency/tests/test_crm_opportunity_currency.py
@@ -11,21 +11,21 @@ class TestCrmOpportunityCurrency(SavepointCase):
         cls.lead = cls.env["crm.lead"].create({"name": "test lead"})
 
     def test_is_same_currency(self):
-        self.lead.customer_currency_id = self.lead.company_id.currency_id
+        self.lead.customer_currency_id = self.lead.company_currency
         self.assertTrue(self.lead.is_same_currency)
-        self.lead.customer_currency_id = self.ref("base.CHF")
+        self.lead.customer_currency_id = self.env.ref("base.CHF")
         self.assertFalse(self.lead.is_same_currency)
 
-    def test_same_currency_planned_revenue_not_updated(self):
-        self.lead.customer_currency_id = self.lead.company_id.currency_id
-        self.lead.planned_revenue = 100
+    def test_same_currency_expected_revenue_not_updated(self):
+        self.lead.customer_currency_id = self.lead.company_currency
+        self.lead.expected_revenue = 100
         self.lead.amount_customer_currency = 124
         self.lead._onchange_currency()
-        self.assertEqual(self.lead.planned_revenue, 100)
+        self.assertEqual(self.lead.expected_revenue, 100)
 
-    def test_different_currency_planned_revenue_updated(self):
-        self.lead.planned_revenue = 100
-        self.lead.customer_currency_id = self.ref("base.CHF")
+    def test_different_currency_expected_revenue_updated(self):
+        self.lead.expected_revenue = 100
+        self.lead.customer_currency_id = self.env.ref("base.CHF")
         self.lead.amount_customer_currency = 124
         self.lead._onchange_currency()
-        self.assertNotEqual(self.lead.planned_revenue, 100)
+        self.assertNotEqual(self.lead.expected_revenue, 100)
