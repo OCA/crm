6246da718f3a4c99373ebd14f07f2c3b347be650
Jose Sosa <josesl@vauxoo.com>
2023-04-18 17:30:09 -0600

    [MIG] crm_lead_currency: Migration to 14.0

--- crm_lead_currency/models/crm_lead.py
+++ crm_lead_currency/models/crm_lead.py
@@ -10,7 +10,7 @@ class CrmLead(models.Model):
     customer_currency_id = fields.Many2one(
         string="Customer currency",
         comodel_name="res.currency",
-        default=lambda self: self.env.user.company_id.currency_id,
+        default=lambda self: self.env.company.currency_id,
     )
     amount_customer_currency = fields.Monetary(
         string="Customer amount",
@@ -23,29 +23,27 @@ class CrmLead(models.Model):
 
     @api.onchange("customer_currency_id", "amount_customer_currency")
     def _onchange_currency(self):
-        self.planned_revenue = self.get_revenue_in_company_currency()
+        self.expected_revenue = self.get_revenue_in_company_currency()
 
-    @api.multi
     def get_revenue_in_company_currency(self):
-        """Compute the planned revenue in the company currency.
+        """Compute the expected revenue in the company currency.
 
         If the customer currency is different than the company currency,
-        the planned revenue is computed in the company currency.
+        the expected revenue is computed in the company currency.
         """
         self.ensure_one()
         if self.is_same_currency:
-            return self.planned_revenue
+            return self.expected_revenue
         return self.customer_currency_id._convert(
             self.amount_customer_currency or 0,
             self.company_currency,
-            self.env.user.company_id,
+            self.env.company,
             fields.Datetime.now(),
         )
 
-    @api.multi
-    @api.depends("customer_currency_id", "company_id.currency_id")
+    @api.depends("customer_currency_id", "company_currency")
     def _compute_is_same_currency(self):
         for lead in self:
             lead.is_same_currency = lead.customer_currency_id == (
-                lead.company_currency or self.env.user.company_id.currency_id
+                lead.company_currency or self.env.company.currency_id
             )
