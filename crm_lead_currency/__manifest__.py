9cc6042995f3ac15aded119fe01aed1b9007c1d0
keylor2906 <josemanuel@vauxoo.com>
2023-06-20 12:07:57 -0600

    [MIG] crm_lead_currency: Migration to 16.0

--- crm_lead_currency/__manifest__.py
+++ crm_lead_currency/__manifest__.py
@@ -8,7 +8,7 @@
     "maintainers": [
         "luisg123v",
     ],
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Camptocamp SA,Odoo Community Association (OCA),Vauxoo",
     "website": "https://github.com/OCA/crm",
--- crm_lead_currency/tests/test_crm_opportunity_currency.py
+++ crm_lead_currency/tests/test_crm_opportunity_currency.py
@@ -1,10 +1,10 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
 
-from odoo.tests.common import SavepointCase
+from odoo.tests import TransactionCase
 
 
-class TestCrmOpportunityCurrency(SavepointCase):
+class TestCrmOpportunityCurrency(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
