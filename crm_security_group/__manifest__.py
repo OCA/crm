fe308ea2dbb7b3970e27d8320aa6df7f7b102597
Víctor Martínez <victor.martinez@tecnativa.com>
2023-06-22 12:45:57 +0200

    [FIX] crm_security_group: Add ACL from crm.lead.lost to group_crm_own_leads
    TT44050
    
    crm_security_group 15.0.1.2.0

--- crm_security_group/__manifest__.py
+++ crm_security_group/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "CRM Only Security Groups",
     "summary": "Add new group in Sales to show only CRM",
-    "version": "15.0.1.1.0",
+    "version": "15.0.1.2.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/crm",
     "author": "Tecnativa, Odoo Community Association (OCA)",
