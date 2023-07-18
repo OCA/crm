9e26b028dc64ec6f919b80585af99304522f8531
Emilio Pascual <emilio@moduon.team>
2023-07-18 18:00:07 +0200

    [15.0][ADD] Module added to create task from lead/opportunities
    @moduon MT-3175
    
    Co-authored-by: Rafael Blasco <8736623+rafaelbn@users.noreply.github.com>
    Co-authored-by: Jairo Llopis <973709+yajo@users.noreply.github.com>

---
+++ crm_project_task/__manifest__.py
@@ -0,0 +1,26 @@
+# Copyright 2023 Moduon Team S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "CRM Project Task",
+    "summary": "Create tasks from lead or opportunity",
+    "version": "15.0.1.0.1",
+    "development_status": "Alpha",
+    "category": "Sales/CRM",
+    "website": "https://github.com/OCA/crm",
+    "author": "Moduon, Odoo Community Association (OCA)",
+    "maintainers": ["EmilioPascual"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "crm",
+        "project",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_config_settings.xml",
+        "wizards/crm_create_task.xml",
+        "views/crm_lead.xml",
+    ],
+}
---
+++ crm_project_task/readme/CONFIGURE.rst
@@ -0,0 +1,6 @@
+To configure this module, you need to:
+
+* Go to Project
+* Create a project with right configuration in order that all salesman have access (usually all employees)
+* Go to CRM > Configuration > Project
+* Set crm default project
