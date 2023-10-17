bc321fcd44cde29df0481517adab44d4c0b37875
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-17 16:11:52 +0200

    [IMP] crm_project: test performance improvement

--- crm_project/tests/test_crm_project.py
+++ crm_project/tests/test_crm_project.py
@@ -9,6 +9,16 @@ class TestCrmProject(common.TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        cls.env = cls.env(
+            context=dict(
+                cls.env.context,
+                mail_create_nolog=True,
+                mail_create_nosubscribe=True,
+                mail_notrack=True,
+                no_reset_password=True,
+                tracking_disable=True,
+            )
+        )
         cls.lead = cls.env["crm.lead"].create(
             {
                 "name": "Test lead",
