c9be6cfda95614d4676a8740e7d3dc5cec1a645b
remi-filament <30716308+remi-filament@users.noreply.github.com>
2023-12-20 14:27:53 +0100

    [MIG] crm_stage_probability: Migration to 16.0

--- crm_stage_probability/__manifest__.py
+++ crm_stage_probability/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "CRM stage probability",
     "summary": "Define fixed probability on the stages",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "development_status": "Alpha",
     "category": "Sales/CRM",
     "website": "https://github.com/OCA/crm",
