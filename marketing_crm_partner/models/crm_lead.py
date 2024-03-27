fcc6b8ca624c22be5cc9772b9a3f7e64f1e41dde
yibudak <yibudak@gmail.com>
2024-03-27 09:04:47 +0300

    [FIX] marketing_crm_partner: inherited function args

--- marketing_crm_partner/models/crm_lead.py
+++ marketing_crm_partner/models/crm_lead.py
@@ -11,12 +11,12 @@ class CRMLead(models.Model):
     _inherit = "crm.lead"
 
     @api.model
-    def _prepare_customer_values(self, name, is_company, parent_id=False):
+    def _prepare_customer_values(self, partner_name, is_company=False, parent_id=False):
         """Populate marketing fields in partner."""
-        res = super()._prepare_customer_values(name, is_company, parent_id)
+        res = super()._prepare_customer_values(partner_name, is_company, parent_id)
         # We use self.env['utm.mixin'] for not losing possible overrides
         # see https://github.com/odoo/odoo/blob/
-        # e5c8071484c883bf78478a39ef2120bcd8f2442d/addons/utm/models/utm.py#L51
+        # 51833e4735fb0b761d3cd2867dfd166813469e70/addons/utm/models/utm_mixin.py#L52
         for _key, field, _cookie in self.env["utm.mixin"].tracking_fields():
             res[field] = self[field].id
         return res
