5e63682dd746832fc559958e67335216ece73d10
Vincent Van Rossem <vincent.vanrossem@camptocamp.com>
2024-06-13 16:59:21 +0200

    Replace 'Camptocamp SA' with 'Camptocamp'

--- crm_lead_currency/__manifest__.py
+++ crm_lead_currency/__manifest__.py
@@ -7,7 +7,7 @@
         On leads/opportunities, add the amount in the customer's currency.""",
     'version': '12.0.1.0.1',
     'license': 'AGPL-3',
-    'author': 'Camptocamp SA,Odoo Community Association (OCA)',
+    'author': 'Camptocamp,Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/crm',
     'depends': [
         'crm',
