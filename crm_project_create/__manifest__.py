a471ad11f11ca9c8ae8c8d5d956d449f7a7e52ac
Florent THOMAS <florent.thomas@mind-and-go.com>
2024-10-20 18:12:52 +0200

    [IMP] crm_project_create: pre-commit auto fixes

--- crm_project_create/__manifest__.py
+++ crm_project_create/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "CRM Project Create",
     "summary": "Allow create projects from lead/opportunity",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "development_status": "Alpha",
     "category": "Sales/CRM",
     "website": "https://github.com/OCA/crm",
