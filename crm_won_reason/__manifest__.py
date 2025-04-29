665f160c7e7a4003c43c62af94ef46d4c3ad5720
AndreuSempere <andreusm1707@gmail.com>
2025-04-29 13:46:44 +0200

    [MIG] crm_won_reason: Migration to 17.0

--- crm_won_reason/__manifest__.py
+++ crm_won_reason/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "CRM won reason",
-    "version": "15.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Customer Relationship Management",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/crm",
--- crm_won_reason/tests/test_crm_lead_reason.py
+++ crm_won_reason/tests/test_crm_lead_reason.py
@@ -36,14 +36,14 @@ class TestCrmLeadReason(TransactionCase):
 
     def test_lost_reason(self):
         crm_lead = self.crm_lead_model.create({"name": "Testing lead lost reason"})
-        crm_lead.action_set_lost(lost_reason=self.lost_reason)
+        crm_lead.action_set_lost(lost_reason_id=self.lost_reason.id)
         self.assertFalse(crm_lead.stage_id.is_won)
-        self.assertEqual(crm_lead.lost_reason.name, self.lost_reason.name)
+        self.assertEqual(crm_lead.lost_reason_id.name, self.lost_reason.name)
 
     def test_unspecified_reason(self):
         crm_lead = self.crm_lead_model.create(
             {"name": "Testing lead unspecified reason"}
         )
-        crm_lead.action_set_lost(lost_reason=self.unspecified_reason)
+        crm_lead.action_set_lost(lost_reason_id=self.unspecified_reason)
         self.assertFalse(crm_lead.stage_id.is_won)
-        self.assertEqual(crm_lead.lost_reason.name, self.unspecified_reason.name)
+        self.assertEqual(crm_lead.lost_reason_id.name, self.unspecified_reason.name)
