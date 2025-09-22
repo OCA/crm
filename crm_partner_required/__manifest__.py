958fdb90300c43acd2e2f55c6f6eadf2b8a73441
Joel Estrada <joel@moduon.team>
2025-09-22 15:01:46 +0200

    [MIG] crm_partner_required: Migration to 18.0
    MT-11508 @moduon

--- crm_partner_required/__manifest__.py
+++ crm_partner_required/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "CRM Partner Required",
     "summary": "Field partner required in the opportunity",
-    "version": "16.0.1.0.1",
+    "version": "18.0.1.0.1",
     "development_status": "Alpha",
     "category": "Sales/CRM",
     "website": "https://github.com/OCA/crm",
